3b8b12b88369e2cceb95df7d544d3ce14acae0f2
EmilyBourne <louise.bourne@gmail.com>
2017-02-27 12:41:54 +0100

    added choice of reference frame

--- Drehscheibe-Corioliskraft/main.py
+++ Drehscheibe-Corioliskraft/main.py
@@ -1,40 +1,51 @@
 from __future__ import division
 from bokeh.plotting import figure
 from bokeh.layouts import column, row
-from bokeh.models import ColumnDataSource, Slider, Button, Toggle, Arrow, OpenHead
+from bokeh.models import ColumnDataSource, Slider, Button, Toggle, Arrow, OpenHead, RadioGroup
 from bokeh.io import curdoc
 from math import pi, sin, cos, radians, sqrt, atan2
 
 ## Create all required variables
-circle_axis_circ_coords=dict(rho=[[],[]],phi=[[],[]])
+circle_axis_phi=[]
 circle_axis_source=ColumnDataSource(data=dict(x=[[],[]],y=[[],[]]))
+room_axis_phi=[]
+room_axis_source=ColumnDataSource(data=dict(x=[[],[]],y=[[],[]]))
 mass_source=ColumnDataSource(data=dict(x=[],y=[]))
 mass_path_circ_coords=dict(rho=[],phi=[])
 mass_path_source=ColumnDataSource(data=dict(x=[],y=[]))
 mass_lab_path_source=ColumnDataSource(data=dict(x=[],y=[]))
+mass_lab_path_circ=dict(rho=[],phi=[])
 mass_pos=[8,3.0*pi/4.0];
 Omega=radians(2)
 speed=[2,-2];
+OSpeed=[2,-2];
 v0_source=ColumnDataSource(data=dict(xStart=[],xEnd=[],yStart=[],yEnd=[]))
 startPos=[]
 Active=False
+OnTable=False
 
 ## Functions
 # give initial values (called by stop and reset, therefore does not re-initialise arrow)
 def initialise ():
-    global mass_source, mass_pos, mass_lab_path_source, mass_path_circ_coords, mass_path_source
-    global circle_axis_circ_coords, circle_axis_source, startPos
+    global mass_source, mass_pos, mass_lab_path_source, mass_path_circ_coords, mass_path_source, mass_lab_path_circ
+    global circle_axis_source, startPos, circle_axis_phi, room_axis_phi, room_axis_source, speed, OSpeed
+    # give room axis coordinates
+    room_axis_phi=[0,pi/2.0]
+    room_axis_source.data=dict(x=[[-12, 12], [0,0]],y=[[0,0], [-12,12]])
     # give mass position in circular coordinates
     mass_pos=[8,3.0*pi/4.0];
     # give mass position in cartesian coordinates (stored in startPos to prevent unnecessary function calls)
     mass_source.data=dict(x=[startPos[0]],y=[startPos[1]])
     # clear old paths
-    mass_path_source.data=dict(x=[],y=[])
+    mass_path_source.data=dict(x=[startPos[0]],y=[startPos[1]])
     mass_lab_path_source.data=dict(x=[startPos[0]],y=[startPos[1]])
+    mass_lab_path_circ=dict(rho=[8],phi=[3.0*pi/4.0])
     mass_path_circ_coords=dict(rho=[8],phi=[3.0*pi/4.0])
     # reset disk orientation in circular and cartesian coordinates
-    circle_axis_circ_coords=dict(rho=[[8,8],[8,8]],phi=[[0,pi],[pi/2.0,-pi/2.0]])
+    circle_axis_phi=[0,pi/2.0]
     circle_axis_source.data=dict(x=[[-8,8],[0,0]],y=[[0,0],[8,-8]])
+    # reset speed if it has moved with the room
+    speed=OSpeed
     # set up directional arrow
     v0_source.data=dict(xStart=[startPos[0]-speed[0]],xEnd=[startPos[0]],
         yStart=[startPos[1]-speed[1]],yEnd=[startPos[1]])
@@ -61,13 +72,24 @@ def circ_to_cart (rho,phi):
     return X,Y
 
 def move():
-    global mass_source, mass_pos, speed, mass_lab_path_source
+    global mass_source, mass_pos, speed, mass_lab_path_source, mass_lab_path_circ, mass_path_circ_coords
     # use mass_pos because bokeh was occasionally modifying mass_source
     # no idea why!
     [X,Y]=circ_to_cart_coord(mass_pos[0],mass_pos[1])
     # update mass position
     X+=0.1*speed[0]
     Y+=0.1*speed[1]
+    # save new position
+    [mass_pos[0],mass_pos[1]]=cart_to_circ(X,Y)
+    # if in table referential spin
+    if (OnTable):
+        end=[X+0.1*speed[0],Y+0.1*speed[1]]
+        [rho,phi]=cart_to_circ(end[0],end[1])
+        phi-=Omega
+        mass_pos[1]-=Omega
+        [X,Y]=circ_to_cart_coord(mass_pos[0],mass_pos[1])
+        [X2,Y2]=circ_to_cart_coord(rho,phi)
+        speed=[10*(X2-X),10*(Y2-Y)]
     mass_source.data=dict(x=[X],y=[Y])
     # copy mass_lab_path_source so the update works
     X2=list(mass_lab_path_source.data['x'])
@@ -77,31 +99,62 @@ def move():
     Y2.append(Y)
     # update path in laboratory referential system
     mass_lab_path_source.data=dict(x=X2,y=Y2)
-    # save new position
-    mass_pos=cart_to_circ(X,Y);
-    # spin the disk and all associated objects
-    spin(mass_pos)
-    
-def spin(mass_pos):
-    global circle_axis_circ_coords, Omega, mass_path_source, Active
-    # give appearance of rotating main disk by rotating axes
-    circle_axis_circ_coords['phi'][0][0]+=Omega
-    circle_axis_circ_coords['phi'][0][1]+=Omega
-    circle_axis_circ_coords['phi'][1][0]+=Omega
-    circle_axis_circ_coords['phi'][1][1]+=Omega
-    # calculate cartesian coordinates after rotation from cylindrical coordinates
-    [X1,Y1]=circ_to_cart(circle_axis_circ_coords['rho'][0],circle_axis_circ_coords['phi'][0])
-    [X2,Y2]=circ_to_cart(circle_axis_circ_coords['rho'][1],circle_axis_circ_coords['phi'][1])
-    circle_axis_source.data=dict(x=[X1, X2],y=[Y1, Y2])
-    # rotate old positions of mass with disk
-    for i in range(0,len(mass_path_circ_coords['phi'])):
-        mass_path_circ_coords['phi'][i]+=Omega
-    # add new position of mass to list
+    mass_lab_path_circ['rho'].append(mass_pos[0])
+    mass_lab_path_circ['phi'].append(mass_pos[1])
+    # update path in disk referential system
     mass_path_circ_coords['rho'].append(mass_pos[0])
     mass_path_circ_coords['phi'].append(mass_pos[1])
+    # spin the disk and all associated objects
+    if (OnTable):
+        spinRoom(mass_pos)
+    else:
+        spinTable(mass_pos)
+    
+def spinTable(mass_pos):
+    global Omega, mass_path_source, Active, circle_axis_phi
+    # give appearance of rotating main disk by rotating axes
+    circle_axis_phi[0]+=Omega
+    circle_axis_phi[1]+=Omega
+    # calculate cartesian coordinates after rotation from cylindrical coordinates
+    [X1,Y1]=circ_to_cart_coord(8,circle_axis_phi[0])
+    [X2,Y2]=circ_to_cart_coord(8,circle_axis_phi[1])
+    circle_axis_source.data=dict(x=[[X1,-X1], [X2,-X2]],y=[[Y1,-Y1], [Y2,-Y2]])
+    # rotate old positions of mass with disk
+    for i in range(0,len(mass_path_circ_coords['phi'])-1):
+        mass_path_circ_coords['phi'][i]+=Omega
     # calculate cartesian coordinates after rotation from cylindrical coordinates
     [X,Y]=circ_to_cart(mass_path_circ_coords['rho'],mass_path_circ_coords['phi'])
     mass_path_source.data=dict(x=X,y=Y)
+    # if ball has exited disk stop the animation
+    if (mass_pos[0]>8):
+        curdoc().remove_periodic_callback(move)
+        Active=False
+
+def spinRoom (mass_pos):
+    global room_axis_phi, Omega, room_axis_source, mass_path_source, Active, circle_axis_phi#, speed
+    # give appearance of rotating room by rotating axes
+    room_axis_phi[0]-=Omega
+    room_axis_phi[1]-=Omega
+    # calculate cartesian coordinates after rotation from cylindrical coordinates
+    [X1,Y1]=circ_to_cart_coord(15,room_axis_phi[0])
+    [X2,Y2]=circ_to_cart_coord(15,room_axis_phi[1])
+    room_axis_source.data=dict(x=[[X1,-X1], [X2,-X2]],y=[[Y1,-Y1], [Y2,-Y2]])
+    # calculate cartesian coordinates on disk
+    [X,Y]=circ_to_cart(mass_path_circ_coords['rho'],mass_path_circ_coords['phi'])
+    mass_path_source.data=dict(x=X,y=Y)
+    # rotate old positions of mass with room
+    for i in range(0,len(mass_lab_path_circ['phi'])-1):
+        mass_lab_path_circ['phi'][i]-=Omega
+    # calculate cartesian coordinates after rotation from cylindrical coordinates
+    [X,Y]=circ_to_cart(mass_lab_path_circ['rho'],mass_lab_path_circ['phi'])
+    mass_lab_path_source.data=dict(x=X,y=Y)
+    # move v0 arrow
+    [rho,phi]=cart_to_circ(v0_source.data['xStart'][0],v0_source.data['yStart'][0])
+    start=circ_to_cart_coord(rho,phi-Omega)
+    [rho,phi]=cart_to_circ(v0_source.data['xEnd'][0],v0_source.data['yEnd'][0])
+    end=circ_to_cart_coord(rho,phi-Omega)
+    v0_source.data=dict(xStart=[start[0]],xEnd=[end[0]],
+            yStart=[start[1]],yEnd=[end[1]])
     # if ball has exited disk stop the animation
     if (mass_pos[0]>8):
         curdoc().remove_periodic_callback(move)
@@ -115,23 +168,25 @@ def rotation_speed(attrname, old, new):
 
 # Set v0
 def particle_speed_x (attrname,old,new):
-    global speed, Active
+    global speed, Active, OSpeed
     if (not Active):
-        speed=[new,speed[1]];
+        speed=[new,OSpeed[1]];
+        OSpeed=speed
         v0_source.data=dict(xStart=[startPos[0]-speed[0]],xEnd=[startPos[0]],
             yStart=[startPos[1]-speed[1]],yEnd=[startPos[1]])
-    elif (new!=speed[0]):
+    elif (new!=OSpeed[0]):
         global v0_input_x
         v0_input_x.value=speed[0]
 
 # Set v0
 def particle_speed_y (attrname,old,new):
-    global speed
+    global speed, OSpeed
     if (not Active):
-        speed=[speed[0],new];
+        speed=[OSpeed[0],new];
+        OSpeed=speed
         v0_source.data=dict(xStart=[startPos[0]-speed[0]],xEnd=[startPos[0]],
             yStart=[startPos[1]-speed[1]],yEnd=[startPos[1]])
-    elif (new!=speed[1]):
+    elif (new!=OSpeed[1]):
         global v0_input_y
         v0_input_y.value=speed[1]
 
@@ -188,6 +243,10 @@ def play ():
         curdoc().add_periodic_callback(move, 100)
         Active=True
 
+def chooseRef (attrname, old, new):
+    global OnTable
+    OnTable = (new==1);
+
 # initialise the start position of the ball
 # (these lines are not in initialise() as it is unnecessary to call "circ_to_cart" at every "stop" or "reset")
 [X,Y]=circ_to_cart([mass_pos[0]],[mass_pos[1]]);
@@ -223,25 +282,32 @@ stop_button.on_click(stop)
 play_button = Button(label="Play", button_type="success")
 play_button.on_click(play)
 
+## Create choice of referential button
+Referential_button = RadioGroup(labels=["Bezugssystem des Raum(Room reference frame)",
+    "Bezugssystem der Scheibe(Disk reference frame)"], active=0)
+Referential_button.on_change('active',chooseRef)
+
 ## create drawing
 p=figure(title="Drehscheibe-Corioliskraft (Coriolis Force)", tools="", x_range=(-12,12), y_range=(-12,12))
 p.title.text_font_size="20pt"
 p.axis.visible = False
 p.grid.visible = False
 p.outline_line_color = None
+# draw room
+p.multi_line('x','y',source=room_axis_source,line_color="#003359")
 # draw disk
-p.ellipse(0,0,16,16,fill_color="#74CFED",line_color=None)
-p.multi_line('x','y',source=circle_axis_source,line_color="#0099CC")
+p.ellipse(0,0,16,16,fill_color="#98C6EA",line_color=None)
+p.multi_line('x','y',source=circle_axis_source,line_color="#0065BD")
 # draw old positions of the ball in circular and cartesian coordinates
-p.line(x='x',y='y',source=mass_path_source,line_color="red")
-p.line(x='x',y='y',source=mass_lab_path_source,line_color="green")
+p.line(x='x',y='y',source=mass_path_source,line_color="#E37222")
+p.line(x='x',y='y',source=mass_lab_path_source,line_color="#A2AD00")
 # draw ball
-p.ellipse(x='x',y='y',width=0.5,height=0.5,source=mass_source,fill_color="red",line_color=None)
+p.ellipse(x='x',y='y',width=0.5,height=0.5,source=mass_source,fill_color="#E37222",line_color=None)
 # draw directional arrow
 arrow_glyph = Arrow(end=OpenHead(line_color="black",line_width=2),
     x_start='xStart', y_start='yStart', x_end='xEnd', y_end='yEnd',source=v0_source)
 p.add_layout(arrow_glyph)
 
 ## Send to window
-curdoc().add_root(row(p,column(Omega_input,v0_input_x,v0_input_y,reset_button,pause_button,stop_button,play_button)))
+curdoc().add_root(row(p,column(Omega_input,v0_input_x,v0_input_y,reset_button,pause_button,stop_button,play_button,Referential_button)))
 curdoc().title = "Drehscheibe-Corioliskraft"
