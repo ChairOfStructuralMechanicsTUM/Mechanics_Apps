9003bd468bfbfd5ec859173fe44c3c738443e388
EmilyBourne <louise.bourne@gmail.com>
2017-03-19 18:08:09 +0100

    removed unnecessary import

--- Zugversuch/main.py
+++ Zugversuch/main.py
@@ -1,7 +1,7 @@
 from bokeh.plotting import figure
 from bokeh.layouts import column, row
 from bokeh.models import ColumnDataSource, Slider, LabelSet, Arrow, OpenHead
-from bokeh.io import curdoc#, push_session
+from bokeh.io import curdoc
 from numpy import loadtxt
 from time import sleep
 
