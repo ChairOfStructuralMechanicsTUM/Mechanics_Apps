fb2f0f4c33704906b66dc199033e9a2c7544942c
abeduplaa <abrahamduplaa@gmail.com>
2017-04-16 13:14:45 +0200

    added cantilever, fixed moment

---
+++ Balken_AD/cantilever.py
@@ -0,0 +1,1 @@
+#cantilever stuff
--- Balken_AD/main.py
+++ Balken_AD/main.py
@@ -33,8 +33,11 @@ f2_arrow_source = ColumnDataSource(data=dict(xS=[], xE=[], yS=[], yE=[], lW = []
 f1_arrow_source = ColumnDataSource(data=dict(xS=[], xE=[], yS=[], yE=[], lW = []))
 #label_source:
 labels_source = ColumnDataSource(data=dict(x=[] , y=[],name = []))
-#Position 2 Triangle source:
-f2_triangle_source = ColumnDataSource(data=dict(x= [], y= [], size = []))
+#Triangle source:
+triangle_source = ColumnDataSource(data=dict(x= [], y= [], size = []))
+#Cantilever rectangle source:
+quad_source = ColumnDataSource(data=dict(top= [], bottom= [],left = [], right =[]))
+segment_source = ColumnDataSource(data=dict(x0= [], y0= [],x1 = [], y1 =[]))
 #Sliders:
 p_loc_slide= Slider(title="Lastposition",value= p_loci,start = x0, end = xf, step = 1/resol)
 p_mag_slide = Slider(title="Lastamplitude", value=p_mag, start=-2*p_mag, end=2*p_mag, step=1)
@@ -56,21 +59,62 @@ def Fun_Moment(p,a,b,l):
 #FUNCTION: Calculation of deflection:
 def Fun_Deflection(a,b,l,p,x):
     ynew = []
-    tri_defl = []
-    for i in range(0,resol):
+    ynew1 = []
+    ynew2 = []
+    for i in range(0,int(l*10)):
         if x[i] < a:
             dy = ( ( p * b * x[i]) / (6 * E * I * l) ) * ( (l**2) - (b**2) - (x[i]**2) )
         elif x[i] == a:
             dy = ( p * (a**2) * (b**2) ) / (3 * E * I * l)
         elif x[i] > a and x[i] <= l:
             dy = ( (p * a * (l-x[i]) ) / (6 * E * I * l) ) * ( (2*l*x[i]) - (x[i]**2) - (a**2) )
-        elif x[i] > l:
-            dy = ( (p * a * b * x[i]) / (6 * E * I * l) ) * (l + a)
-        ynew.append(dy)
+        ynew1.append(dy)
+
+    new_range = int(resol - l*10)
+    for i in range(0,new_range):
+        dy1 = -1 *( ( (p * a * b * x[i]) / (6 * E * I * l) ) * (l + a) )
+        ynew2.append(dy1)
+
+    ynew = ynew1 + ynew2
     return ynew
 
+
+
+#FUNCTION: Cantilever Deflection function:
+def Fun_C_Deflection(p,a,x):
+    '''Calculates the deflection of the beam when it is cantilever'''
+    ynew = []
+    for i in range(0,resol):
+        if x[i] < a:
+            dy = (  ( p * ( ( xf - x[i])**2 ) ) / (6 * E * I) ) * ( (3*a) - xf + x[i] )
+        elif x[i] == a:
+            dy = ( p * (a**3) ) / (3 * E * I)
+        elif x[i] > a:
+            dy = (  ( p * (a**2) ) / (6 * E * I)  ) * ( (3*xf) - (3*x[i]) - a )
+        ynew.append(dy)
+
+    return ynew
+
+#FUNCTION: Cantilever function:
+#When position 2 is 0, this function is called:
+def Fun_Cantilever():
+    triangle_source.data = dict(x = [], y = [], size = [])
+    f1_arrow_source.data = dict(xS= [], xE= [], yS= [], yE=[], lW = [])
+    f2_arrow_source.data = dict(xS= [], xE= [], yS= [], yE=[], lW = [])
+    top = 2
+    bottom  = -top
+    left = -1
+    right = -left
+    clines = 40
+    quad_source.data = dict(top = [top], bottom = [bottom], left = [left] , right = [right])
+    xseg = np.ones(clines) * left
+    yseg = np.linspace(bottom,top-0.2,clines)
+    x1seg = np.ones(clines) * right
+    y1seg = np.linspace(bottom+0.2,top,clines)
+    segment_source.data = dict(x0= xseg, y0= yseg,x1 = x1seg, y1 =y1seg)
 #FUNCTION: Update:
 def Fun_Update(attrname, old, new):
+    #values used by both cases:
     my_line.glyph.line_width = 40/lth_slide.value
     a = p_loc_slide.value
     f2_coord = f2_loc_slide.value
@@ -78,71 +122,120 @@ def Fun_Update(attrname, old, new):
     p_coord = p_loc_slide.value
     p_mag = p_mag_slide.value
     l = f2_coord
-    f1_mag = Fun_F(p_mag_slide.value,b,l)
-    f2_mag = Fun_F(p_mag_slide.value,a,l)
+    x1 = xf - l
 
-    if checkbox.active == [0]:
-        ynew = Fun_Deflection(a,b,l,p_mag,plot_source.data['x'])
-        plot_source.data = dict(x = np.linspace(x0,xf,resol), y = ynew)
-    elif checkbox.active == []:
-        plot_source.data= dict( x = np.linspace(x0,xf,resol) , y = np.ones(resol) * 0)
-    elif checkbox.active == [1]:
-        checkbox.active = []
-    elif checkbox.active == [0,1]:
-        ynew = Fun_Deflection(a,b,l,p_mag,plot_source.data['x'])
-        plot_source.data = dict(x = np.linspace(x0,xf,resol), y = ynew)
-        print 'this would be mit schub'
+
+    if f2_coord == 0:
+        xcan = x0 + 1
+        Fun_Cantilever()
+
+        if (p_mag==0):
+            p_arrow_source.data = dict(xS=[], xE=[], yS=[], yE=[], lW = [])
+            labels_source.data = dict(x = [] , y = [], name = [])
+        elif (p_mag<0):
+            p_arrow_source.data = dict(xS= [p_coord], xE= [p_coord], yS= [1-(p_mag/200.0)], yE=[1], lW = [abs(p_mag/40.0)] )
+            labels_source.data = dict(x = [p_coord] , y = [1],name = ['F'])
+        else:
+            p_arrow_source.data = dict(xS= [p_coord], xE= [p_coord], yS= [-1-(p_mag/200.0)], yE=[-1], lW = [abs(p_mag/40.0)] )
+            labels_source.data = dict(x = [p_coord] , y = [-1],name = ['F'])
+
+        #cantilever moment calculation:
+        #max moment at fixed end x0. Moment max = P*l
+        m_max = p_mag * l
+        mom_source.data = dict(x=[xcan,l,xf] , y=[m_max,0,0])
+        shear_source.data = dict(x=[xcan,l,l,xf], y=[-p_mag,-p_mag,0,0])
+
+        if checkbox.active == [0]:
+            ynew = Fun_C_Deflection(p_mag,a,plot_source.data['x'])
+            plot_source.data = dict(x = np.linspace(x0,xf,resol), y = ynew)
+        elif checkbox.active == []:
+            plot_source.data= dict( x = np.linspace(x0,xf,resol) , y = np.ones(resol) * 0)
+        elif checkbox.active == [1]:
+            checkbox.active = []
+        elif checkbox.active == [0,1]:
+            ynew = Fun_C_Deflection(p_mag,a,plot_source.data['x'])
+            plot_source.data = dict(x = np.linspace(x0,xf,resol), y = ynew)
+            print 'this would be mit schub'
+        else:
+            print 'fatal error'
+
+
+
     else:
-        print 'fatal error'
 
-    move_tri = -0.25
-    f2_triangle_source.data = dict(x = [0.0,f2_loc_slide.value], y = [0+move_tri, 0+move_tri], size = [20,20])
 
-    #moment and shear:
-    m_max = Fun_Moment(p_mag_slide.value,a,b,l)
-    if (l >= a):
-        mom_source.data = dict(x=[0,a,l] , y=[0,m_max,0])
-        shear_source.data = dict(x=[0,a,a,l], y=[-f1_mag,-f1_mag,f2_mag,f2_mag])
-    else:
-        mom_source.data = dict(x=[0,l,a] , y=[0,m_max,0])
-        shear_source.data = dict(x=[0,l,l,a], y=[-f1_mag,-f1_mag,p_mag,p_mag])
 
-    #p_arrow and labels:
-    if (p_mag==0):
-        p_arrow_source.data = dict(xS=[], xE=[], yS=[], yE=[], lW = [])
-        labels_source.data = dict(x = [] , y = [], name = ['F','A','B'])
 
-    elif (p_mag<0):
-        p_arrow_source.data = dict(xS= [p_coord], xE= [p_coord], yS= [1-(p_mag/200.0)], yE=[1], lW = [abs(p_mag/40.0)] )
-        labels_source.data = dict(x = [p_coord,0,f2_coord] , y = [1,move_tri,move_tri],name = ['F','A','B'])
+        quad_source.data = dict(top = [], bottom = [], left = [] , right = [])
+        segment_source.data = dict(x0= [], y0= [],x1 = [], y1 =[])
 
-    else:
-        p_arrow_source.data = dict(xS= [p_coord], xE= [p_coord], yS= [-1-(p_mag/200.0)], yE=[-1], lW = [abs(p_mag/40.0)] )
-        labels_source.data = dict(x = [p_coord,0,f2_coord] , y = [-1,move_tri,move_tri],name = ['F','A','B'])
+        f1_mag = Fun_F(p_mag_slide.value,b,l)
+        f2_mag = Fun_F(p_mag_slide.value,a,l)
 
-    #f1_arrow:
-    #if (f1_mag==0):
-        #f1_arrow_source.data = dict(xS=[], xE=[], yS=[], yE=[], lW = [])
-    if (f1_mag<=-p_magi):
-        f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [1-(f1_mag/200.0)], yE=[0.8], lW = [6])
-    elif ( f1_mag>-p_magi ) & ( f1_mag<=0 ):
-        f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [1-(f1_mag/200.0)], yE=[0.8], lW = [abs(f1_mag/40.0)])
-    elif (f1_mag > 0) & ( f1_mag < p_magi ):
-        f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [-1-(f1_mag/200.0)], yE=[-0.8], lW = [abs(f1_mag/40.0)] )
-    else:
-        f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [-1-(f1_mag/200.0)], yE=[-0.8], lW = [6] )
+        if checkbox.active == [0]:
+            ynew = Fun_Deflection(a,b,l,p_mag,plot_source.data['x'])
+            plot_source.data = dict(x = np.linspace(x0,xf,resol), y = ynew)
+        elif checkbox.active == []:
+            plot_source.data= dict( x = np.linspace(x0,xf,resol) , y = np.ones(resol) * 0)
+        elif checkbox.active == [1]:
+            checkbox.active = []
+        elif checkbox.active == [0,1]:
+            ynew = Fun_Deflection(a,b,l,p_mag,plot_source.data['x'])
+            plot_source.data = dict(x = np.linspace(x0,xf,resol), y = ynew)
+            print 'this would be mit schub'
+        else:
+            print 'fatal error'
 
-    #f2_arrow:
-    #if (f2_mag==0):
-    #    f2_arrow_source.data = dict(xS=[], xE=[], yS=[], yE=[])
-    if (f2_mag<=-p_magi):
-        f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [1-(f2_mag/200.0)], yE=[0.8], lW = [6])
-    elif (f2_mag > -p_magi) & (f2_mag <= 0.0):
-        f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [1-(f2_mag/200.0)], yE=[0.8], lW = [abs(f2_mag/40.0)])
-    elif (f2_mag > 0) & ( f2_mag < p_magi ):
-        f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [-1-(f2_mag/200.0)], yE=[-0.8], lW = [abs(f2_mag/40.0)])
-    else:
-        f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [-1-(f2_mag/200.0)], yE=[-0.8], lW = [6])
+        #print plot_source.data['y']
+
+        move_tri = -0.25
+        triangle_source.data = dict(x = [0.0,f2_loc_slide.value], y = [0+move_tri, 0+move_tri], size = [20,20])
+
+        #moment and shear:
+        m_max = Fun_Moment(p_mag_slide.value,a,b,l)
+        if (l >= a):
+            mom_source.data = dict(x=[0,a,l] , y=[0,m_max,0])
+            shear_source.data = dict(x=[0,a,a,l], y=[-f1_mag,-f1_mag,f2_mag,f2_mag])
+        else:
+            mom_source.data = dict(x=[0,l,a] , y=[0,m_max,0])
+            shear_source.data = dict(x=[0,l,l,a], y=[-f1_mag,-f1_mag,p_mag,p_mag])
+
+        #p_arrow and labels:
+        if (p_mag==0):
+            p_arrow_source.data = dict(xS=[], xE=[], yS=[], yE=[], lW = [])
+            labels_source.data = dict(x = [] , y = [], name = ['F','A','B'])
+
+        elif (p_mag<0):
+            p_arrow_source.data = dict(xS= [p_coord], xE= [p_coord], yS= [1-(p_mag/200.0)], yE=[1], lW = [abs(p_mag/40.0)] )
+            labels_source.data = dict(x = [p_coord,0,f2_coord] , y = [1,move_tri,move_tri],name = ['F','A','B'])
+
+        else:
+            p_arrow_source.data = dict(xS= [p_coord], xE= [p_coord], yS= [-1-(p_mag/200.0)], yE=[-1], lW = [abs(p_mag/40.0)] )
+            labels_source.data = dict(x = [p_coord,0,f2_coord] , y = [-1,move_tri,move_tri],name = ['F','A','B'])
+
+        #f1_arrow:
+        #if (f1_mag==0):
+            #f1_arrow_source.data = dict(xS=[], xE=[], yS=[], yE=[], lW = [])
+        if (f1_mag<=-p_magi):
+            f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [1-(f1_mag/200.0)], yE=[0.8], lW = [6])
+        elif ( f1_mag>-p_magi ) & ( f1_mag<=0 ):
+            f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [1-(f1_mag/200.0)], yE=[0.8], lW = [abs(f1_mag/40.0)])
+        elif (f1_mag > 0) & ( f1_mag < p_magi ):
+            f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [-1-(f1_mag/200.0)], yE=[-0.8], lW = [abs(f1_mag/40.0)] )
+        else:
+            f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [-1-(f1_mag/200.0)], yE=[-0.8], lW = [6] )
+
+        #f2_arrow:
+        #if (f2_mag==0):
+        #    f2_arrow_source.data = dict(xS=[], xE=[], yS=[], yE=[])
+        if (f2_mag<=-p_magi):
+            f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [1-(f2_mag/200.0)], yE=[0.8], lW = [6])
+        elif (f2_mag > -p_magi) & (f2_mag <= 0.0):
+            f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [1-(f2_mag/200.0)], yE=[0.8], lW = [abs(f2_mag/40.0)])
+        elif (f2_mag > 0) & ( f2_mag < p_magi ):
+            f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [-1-(f2_mag/200.0)], yE=[-0.8], lW = [abs(f2_mag/40.0)])
+        else:
+            f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [-1-(f2_mag/200.0)], yE=[-0.8], lW = [6])
 
 #initial function:
 def initial():
@@ -158,7 +251,11 @@ def initial():
 ###Main Plot:
 plot = Figure(title="Doppeltgelagerter Balken und Einzellast", x_range=(x0-.5,xf+.5), y_range=(-2.5,2.5))
 my_line=plot.line(x='x', y='y', source=plot_source, color='blue',line_width=20)
-plot.triangle(x='x', y='y', size = 'size', source= f2_triangle_source,color="#99D594", line_width=2)
+plot.triangle(x='x', y='y', size = 'size', source= triangle_source,color="#99D594", line_width=2)
+plot.quad(top='top', bottom='bottom', left='left',
+    right='right', source = quad_source, color="#B3DE69", fill_alpha = 0.5)
+plot.segment(x0='x0', y0='y0', x1='x1',
+          y1='y1', source = segment_source, color="#F4A582", line_width=2)
 plot.axis.visible = False
 plot.outline_line_width = 7
 plot.outline_line_alpha = 0.3
