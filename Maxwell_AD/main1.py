bd438ddadc6ad50f1aa236c4658e000076d26b7d
abeduplaa <abrahamduplaa@gmail.com>
2017-04-27 21:37:40 +0200

    odifications to maxwell

--- Maxwell_AD/main1.py
+++ Maxwell_AD/main1.py
@@ -1,6 +1,7 @@
 from bokeh.plotting import Figure, output_file , show
 from bokeh.models import ColumnDataSource, Slider, LabelSet, OpenHead, Arrow
 from bokeh.layouts import column, row, widgetbox
+from bokeh.models.widgets import Toggle
 from bokeh.io import curdoc
 import numpy as np
 
@@ -11,22 +12,30 @@ import numpy as np
 
 class Frame(object):
     def __init__(self,name):
-        self.pts = ColumnDataSource(data=dict(x = [], y = [] ))
-        self.p_mag = 0
-        self.boundary = 0
-        self.x0 = 0.1
-        self.xf = 0.8
-        self.y0 = 0.1
-        self.yf = 0.6
-        self.name = name
-        self.p_loc = 0
-        self.arrow_source = ColumnDataSource(data=dict(xS=[], xE=[], yS=[], yE=[], lW = []))
+        self.pts            = ColumnDataSource(data=dict(x = [], y = [] ))
+        self.p_mag          = 0
+        self.boundary       = 0
+        self.x0             = 0.1
+        self.xf             = 0.8
+        self.y0             = 0.1
+        self.yf             = 0.6
+        self.name           = name
+        self.p_loc          = 0
+        self.mag_start      = -100
+        self.mag_end        = 100
+        self.mag_val        = 0
+        self.loc_start      = 0
+        self.loc_end        = 100
+        self.loc_val        = 50
+        self.arrow_source   = ColumnDataSource(data=dict(xS=[], xE=[], yS=[], yE=[], lW = []))
+        self.e_s            = ColumnDataSource(data=dict(xS=[], xE=[], yS=[], yE=[], lW = []))
+        self.mag_slider     = Slider(title= self.name + " Kraftbetrag", value=self.mag_val, start=self.mag_start, end=self.mag_end, step=1)
+        self.loc_slider     = Slider(title= self.name + " Kraftposition", value=self.loc_val, start=self.loc_start, end=self.loc_end, step=1)
 
-
-    def set_mag(self,r):
-        self.p_mag = r
-    def set_param(self,s):
-        self.p_loc = s
+    def set_mag(self):
+        self.p_mag = self.mag_slider.value
+    def set_param(self):
+        self.p_loc = self.loc_slider.value
     def get_mag(self):
         return self.p_mag
     def get_param(self):
@@ -55,17 +64,20 @@ f1   = Frame("F1")
 f2   = Frame("F2")
 
 #sliders:
-mag_start = -100
-mag_end = 100
-mag_val = 0
-p1mag_slider = Slider(title=f1.name + " Kraftbetrag", value=mag_val, start=mag_start, end=mag_end, step=1)
-p2mag_slider = Slider(title=f2.name + " Kraftbetrag", value=mag_val, start=mag_start, end=mag_end, step=1)
+#mag_start = -100
+#mag_end = 100
+#mag_val = 0
+#p1mag_slider = Slider(title=f1.name + " Kraftbetrag", value=mag_val, start=mag_start, end=mag_end, step=1)
+#p2mag_slider = Slider(title=f2.name + " Kraftbetrag", value=mag_val, start=mag_start, end=mag_end, step=1)
 
-loc_start = 0
-loc_end = 100
-loc_val = 50
-p1loc_slider = Slider(title=f1.name + " Kraftposition", value=loc_val, start=loc_start, end=loc_end, step=1)
-p2loc_slider = Slider(title=f2.name + " Kraftposition", value=loc_val, start=loc_start, end=loc_end, step=1)
+#Toggle button:
+toggle = Toggle(label="Freeze F1", button_type="success")
+
+#loc_start = 0
+#loc_end = 100
+#loc_val = 50
+#p1loc_slider = Slider(title=f1.name + " Kraftposition", value=loc_val, start=loc_start, end=loc_end, step=1)
+#p2loc_slider = Slider(title=f2.name + " Kraftposition", value=loc_val, start=loc_start, end=loc_end, step=1)
 
 
 def create_orig(o):
@@ -74,6 +86,18 @@ def create_orig(o):
     o.pts.data = dict(x = x, y = y )
 
 #also need a reget function (moves everything back to normal)
+
+def create_prof(f):
+    paramInt = f.get_param()
+    i = f.get_mag()
+    if int(paramInt) < 30:
+        side1(f,paramInt,i)
+
+    elif (int(paramInt)> 30) & (int(paramInt) < 70) :
+        side2(f,paramInt,i)
+
+    elif  int(paramInt) > 70:
+        side3(f,paramInt,i)
 
 def side1(f,paramInt,i):
     x1          = []
@@ -235,11 +259,11 @@ def side3(f,paramInt,i):
     #change arrow:
     if i<0:
         f.arrow_source.data = dict(xS= [0.78+i/arr_scal], xE= [0.78],
-        yS= [0.6 - (paramInt%30)*(1.0/60)], yE=[0.6 - (paramInt%30)*(1.0/60)], lW = [abs(i/arr_lw)] )
+        yS= [0.6 - (paramInt%70)*(1.0/60)], yE=[0.6 - (paramInt%70)*(1.0/60)], lW = [abs(i/arr_lw)] )
 
     elif i>0:
         f.arrow_source.data = dict(xS= [0.82+i/arr_scal], xE= [0.82],
-        yS= [0.6 - (paramInt%30)*(1.0/60)], yE=[0.6 - (paramInt%30)*(1.0/60)], lW = [abs(i/arr_lw)] )
+        yS= [0.6 - (paramInt%70)*(1.0/60)], yE=[0.6 - (paramInt%70)*(1.0/60)], lW = [abs(i/arr_lw)] )
 
 
     d1 = i / FScale
@@ -294,26 +318,178 @@ def side3(f,paramInt,i):
     f.pts.data = dict(x = x, y = y )
 
 
+
+def compute_shift(paramInt1, paramInt2, i):
+    d7 = i / FScale
+
+    localDouble = []
+
+    #create a localdouble Point2d variable
+
+    d1 = paramInt2 / 30.0 * a
+    d2 = a - d1
+    d4 = -(1.0/6.0) * d7 * d1 * b
+    d3 = -d4 + d7 / 2.0 * a * a - d7 / 2.0 * d2 * d2
+    d5 = d7 * d1 * b / 2.0 + d4
+    d8 = -(1.0/6.0) * d7 * (a**3) + d3 * a + (1.0/6.0) * d7 * ( (a - d1) ** 3 )
+    d9 = 0.0
+    d10 = 0.0
+    if (paramInt1 < 30):
+        d6 = paramInt1 / 30.0 * a
+        if (d6 < d1):
+            d9 = -1.0/6.0 * d7 * d6 * d6 * d6 + d3 * d6
+        else:
+            d9 = -1.0/6.0 * d7 * d6 * d6 * d6 + d3 * d6 + 1.0/6.0 * d7 * ((d6 - d1)**3.0)
+
+    if ((30 <= paramInt1) & (paramInt1 <= 70)):
+        d6 = (paramInt1 - 30) / 40.0 * b
+        d10 = 0.5 * d7 * d1 * d6 * d6 - 1.0/6.0 * d7 * d1 / b * (d6**3.0) + d4 * d6
+        d9 = d8
+
+    if (paramInt1 > 70):
+        d6 = (paramInt1 - 70) / 30.0 * a
+        d9 = d5 * d6 + d8
+
+    if (paramInt2 < 30):
+        localDouble = [d9,d10]
+        pass
+
+    d11 = (paramInt2 - 30) / 40.0 * b
+    d4 = d7 / 6.0 * ( ((b - d11)**3.0) - (b - d11) * b)
+    d3 = -d4
+    d5 = d7 / 2.0 * ( (b - d11) * b - ((b - d11)**2.0) ) + d4
+
+    d8 = d3 * a
+    d9 = 0.0
+    d10 = 0.0
+    if (paramInt1 < 30):
+        d6 = paramInt1 / 30.0 * a
+        d9 = d3 * d6
+
+    if ((30 <= paramInt1) & (paramInt1 <= 70)):
+        d6 = (paramInt1 - 30) / 40.0 * b
+        if (d6 < d11):
+            d10 = (d7 / 6.0) *  ( (b - d11) / b ) * d6 * d6 * d6 + d4 * d6
+        else:
+            d10 = d7 / 6.0 * (b - d11) / b * d6 * d6 * d6 + d4 * d6 - d7 / 6.0 * ((d6 - d11)**3.0)
+        d9 = d8
+
+    if (paramInt1 > 70):
+        d6 = (paramInt1 - 70) / 30.0 * a
+        d9 = d5 * d6 + d8
+
+    if ((paramInt2 >= 30) & (paramInt2 <= 70)):
+        localDouble = [d9, d10]
+
+
+    d1  = (100 - paramInt2) / 30.0 * a
+    d2  = a - d1
+    d12 = d7 * d1 / b
+    d4  = 1.0 / b * (d7 * a / 2.0 * b * b - d12 * b * b * b / 6.0)
+    d3  = -d4 - d7 / 2.0 * a * a
+    d5  = -d7 * a * b + d12 * b * b / 2.0 + d4
+    d8  = d7 / 6.0 * a * a * a + d3 * a
+    d9  = 0.0
+    d10 = 0.0
+    if (paramInt1 < 30):
+        d6 = paramInt1 / 30.0 * a
+        d9 = d7 / 6.0 * d6 * d6 * d6 + d3 * d6
+
+    if ((30 <= paramInt1) & (paramInt1 <= 70)):
+        d6  = (paramInt1 - 30) / 40.0 * b
+        d10 = -d7 * a / 2.0 * d6 * d6 + d12 * d6 * d6 * d6 / 6.0 + d4 * d6
+        d9  = d8
+
+    if (paramInt1 > 70):
+        d6 = (paramInt1 - 70) / 30.0 * a
+        if (d6 < d2):
+            d9 = -d7 * d2 / 2.0 * d6 * d6 + d7 / 6.0 * d6 * d6 * d6 + d5 * d6 + d8
+        else:
+            d9 = -d7 * d2 / 2.0 * d6 * d6 + d7 / 6.0 * d6 * d6 * d6 + d5 * d6 - d7 / 6.0 * ((d6 - d2)**3.0) + d8
+
+    if (paramInt2 > 70):
+        localDouble = [d9, d10]
+
+    return localDouble
+
+def create_shift(f1, f2):
+    paramInt1 = f1.get_param()
+    paramInt2 = f2.get_param()
+
+    localDouble1 = compute_shift(paramInt1,paramInt2, f2.get_mag())
+    localDouble2 = compute_shift(paramInt2,paramInt1, f2.get_mag())
+
+    d2 = 0
+    d1 = 0
+    sclr = 5
+    if (paramInt1 < 30):
+        d2 = paramInt1 / 30.0 * 0.5 + 0.1
+        d1 = 0.1
+
+        f1.e_s.data = dict(xS= [ d1 ], xE= [d1 + localDouble1[0]],
+        yS= [d2], yE=[d2], lW = [abs(localDouble1[0]*sclr) ] )
+
+    elif ((30 <= paramInt1) & (paramInt1 <= 70)):
+        d1 = (paramInt1 - 30) / 40.0 * 0.7 + 0.1
+        d2 = 0.6
+
+        f1.e_s.data = dict(xS= [ d1 ], xE= [d1],
+        yS= [d2 + localDouble1[1] ], yE=[d2], lW = [abs(localDouble1[1]*sclr) ] )
+
+    elif (paramInt1 > 70):
+        d1 = 0.8
+        d2 = 0.6 - (paramInt1 - 70) / 30.0 * 0.5
+
+        f1.e_s.data = dict(xS= [ d1], xE= [d1 + localDouble1[0] ],
+        yS= [d2], yE=[d2], lW = [ abs(localDouble1[0]*sclr ) ] )
+
+
+    if (paramInt2 < 30):
+        d2 = paramInt2 / 30.0 * 0.5 + 0.1
+        d1 = 0.1
+        f2.e_s.data = dict(xS= [ d1 + localDouble2[0] ], xE= [d1],
+        yS= [d2], yE=[d2], lW = [abs(localDouble2[0]*sclr ) ] )
+    elif((30 <= paramInt2) & (paramInt2 <= 70)):
+        d1 = (paramInt2 - 30) / 40.0 * 0.7 + 0.1
+        d2 = 0.6
+        f2.e_s.data = dict(xS= [ d1 ], xE= [d1],
+        yS= [d2], yE=[d2 + localDouble1[1] ], lW = [abs(localDouble1[1]*sclr) ] )
+    elif(paramInt2 > 70):
+        d1 = 0.8
+        d2 = 0.6 - (paramInt2 - 70) / 30.0 * 0.5
+
+        f2.e_s.data = dict(xS= [ d1 ], xE= [d1 + localDouble2[0]],
+        yS= [d2], yE=[d2], lW = [abs(localDouble2[0] *sclr) ] )
+
+
+
+
+
 def update_fun(attr,old,new):
-
-    f1.set_mag(p1mag_slider.value)
-    f1.set_param(p1loc_slider.value)
-    mag = f1.get_mag()
-    param   = f1.get_param()
-
-    if int(p1loc_slider.value) < 30:
-        side1(f1,param,mag)
-
-    elif (int(p1loc_slider.value)> 30) & (int(p1loc_slider.value) < 70) :
-        side2(f1,param,mag)
-
-    elif  int(p1loc_slider.value) > 70:
-        side3(f1,param,mag)
+    f1.set_param()
+    f1.set_mag()
+    f2.set_param()
+    f2.set_mag()
+    create_prof(f1)
+    #create_prof(f2)
+    #create_shift(f1,f2)
+    if toggle.active == True:
+        create_prof(f2)
+        create_shift(f1,f2)
 
 
 
-p1loc_slider.on_change('value', update_fun)
-p1mag_slider.on_change('value', update_fun)
+
+#Force 1 sliders:
+f1.loc_slider.on_change('value', update_fun)
+f1.mag_slider.on_change('value', update_fun)
+
+#Force 2 sliders:
+f2.loc_slider.on_change('value', update_fun)
+f2.mag_slider.on_change('value', update_fun)
+
+toggle.on_change('active',update_fun)
+
 
 
 
@@ -323,10 +499,26 @@ create_orig(orig)
 plot = Figure(title="Maxwell", x_range=(-0.1,1.0), y_range=(-0.1,0.8))
 plot.line(x='x', y='y', source=orig.pts, color='#0065BD',line_width=4)
 plot.line(x='x', y='y', source=f1.pts, color="#808080",line_width=5)
+plot.line(x='x', y='y', source=f2.pts, color="yellow",line_width=5)
 
 #P arrow:
-p_arrow_glyph = Arrow(end=OpenHead(line_color="#A2AD00",line_width= 4, size=10),
-    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= "lW", source=f1.arrow_source,line_color="#A2AD00")
-plot.add_layout(p_arrow_glyph)
+p1_arrow_glyph = Arrow(end=OpenHead(line_color="#A2AD00",line_width= 4, size=10),
+    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= "lW", source=f1.arrow_source,line_color="red")
+p2_arrow_glyph = Arrow(end=OpenHead(line_color="#A2AD00",line_width= 4, size=10),
+    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= "lW", source=f2.arrow_source,line_color="green")
+e1_arrow_glyph = Arrow(end=OpenHead(line_color="#A2AD00",line_width= 1, size=5),
+    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= "lW", source=f1.e_s,line_color="red")
+e2_arrow_glyph = Arrow(end=OpenHead(line_color="#A2AD00",line_width= 1, size=5),
+    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= "lW", source=f2.e_s,line_color="red")
 
-curdoc().add_root( row(column(p1mag_slider,p1loc_slider),plot ) )
+
+plot.add_layout(p1_arrow_glyph)
+plot.add_layout(p2_arrow_glyph)
+plot.add_layout(e1_arrow_glyph)
+plot.add_layout(e2_arrow_glyph)
+
+
+
+
+
+curdoc().add_root( row(column(f1.mag_slider,f1.loc_slider,f2.mag_slider,f2.loc_slider, toggle),plot ) )
