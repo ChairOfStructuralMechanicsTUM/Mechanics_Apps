2193696fc574c6c44d6bc0a742febaab15d8271b
Matthias <39389064+m3bert@users.noreply.github.com>
2018-06-14 13:41:48 +0200

    function structure, eigenvalues not hardcoded

--- Bernoulli_beam_vibrations/main.py
+++ Bernoulli_beam_vibrations/main.py
@@ -1,18 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-@author: antonis
+@author: antonis, matthias (re-structured to function design)
 """
 from __future__ import division
 import numpy as np
 import pandas as pd
+from scipy.optimize import brentq # fast numerical solver for roots
 from bokeh.io import curdoc
 from bokeh.layouts import row,column, gridplot
 from bokeh.models import ColumnDataSource,CustomJS, Label,Legend
 from bokeh.models.widgets import Slider,Toggle, Paragraph
 from bokeh.plotting import figure
-from os.path import dirname, join, split 
+from os.path import dirname, join, split, abspath
+# for LaTeX support
+import sys, inspect
+currentdir = dirname(abspath(inspect.getfile(inspect.currentframe())))
+parentdir = join(dirname(currentdir), "shared/")
+sys.path.insert(0,parentdir) 
+from latex_div import LatexDiv
 
+# beam length
 l=1
+
+# first ev eigenvalues for each beam
+ev_total = 4
+
 #beam 1-->simply supported
 beam1 = ColumnDataSource(data=dict(x=[0,1], y=[0,0]))
 #create the boundary conditions for simply supported beam
@@ -36,40 +48,101 @@ beam3=ColumnDataSource(data=dict(x=[0,1], y=[0,0]))
 fixed_cantil=ColumnDataSource(data=dict(x=[0,0,-0.03,0,0,-0.03,0,0,-0.03,0,0,-0.03,0,0,-0.03],y=[2*0.25,-2*0.25,-2*0.3,-2*0.25,-2*0.125,-2*0.175,-2*0.125,0,-2*0.05,-0,+2*0.125,
                                         +2*0.075,+2*0.125,2*0.25,2*0.2]))
 
-#simply supported beam
-N=200
-x1 = np.linspace(0, 1, N)
-y1=-np.sin(np.pi*x1/l)
-source1 = ColumnDataSource(data=dict(x=x1,y=y1))
 
-#beam fixed at both ends
-N=200
-x2=np.linspace(0,1,N)
-y2=-(1/2*(np.cosh(4.73004*x2/l)-np.cos(4.73004*x2/l))-((1/2*(np.cosh(4.73004)-np.cos(4.73004)))/(1/2*(np.sinh(4.73004)-np.sin(4.73004))))*1/2*(np.sinh(4.73004*x2/l)-np.sin(4.73004*x2/l)))
-y2cosh=-(1/2*(np.cosh(4.73004*x2/l)))
-y2sinh=+((1/2*(np.cosh(4.73004)-np.cos(4.73004)))/(1/2*(np.sinh(4.73004)-np.sin(4.73004))))*1/2*(np.sinh(4.73004*x2/l))
-y2cos=-(1/2*(np.cos(4.73004*x2/l)))
-y2sin=-((1/2*(np.cosh(4.73004)-np.cos(4.73004)))/(1/2*(np.sinh(4.73004)-np.sin(4.73004))))*1/2*(np.sin(4.73004*x2/l))
-source2=ColumnDataSource(data=dict(x=x2,y=y2))
-source2cosh=ColumnDataSource(data=dict(x=x2,y=y2cosh))
-source2sinh=ColumnDataSource(data=dict(x=x2,y=y2sinh))
-source2cos=ColumnDataSource(data=dict(x=x2,y=y2cos))
-source2sin=ColumnDataSource(data=dict(x=x2,y=y2sin))
+## pre-compute the eigenvalues for all beams
+def eq_fixed_beam(x):
+    return np.cosh(x)*np.cos(x) - 1.0
 
-#cantilever beam
-N=200
-x3=np.linspace(0,1,N)
-y3=-(1/2*(np.cosh(1.8751*x3/l)-np.cos(1.8751*x3/l))-((1/2*(np.cosh(1.8751)+np.cos(1.8751)))/(1/2*(np.sinh(1.8751)+np.sin(1.8751))))*1/2*(np.sinh(1.8751*x3/l)-
-            np.sin(1.8751*x3/l)))
-y3cosh=-(1/2*(np.cosh(1.8751*x2/l)))
-y3sinh=+((1/2*(np.cosh(1.8751)+np.cos(1.8751)))/(1/2*(np.sinh(1.8751)+np.sin(1.8751))))*1/2*(np.sinh(1.8751*x2/l))
-y3cos=-(1/2*(np.cos(1.8751*x2/l)))
-y3sin=-((1/2*(np.cosh(1.8751)+np.cos(1.8751)))/(1/2*(np.sinh(1.8751)+np.sin(1.8751))))*1/2*(np.sin(1.8751*x2/l))
-source3=ColumnDataSource(data=dict(x=x3,y=y3))
-source3cosh=ColumnDataSource(data=dict(x=x3,y=y3cosh))
-source3sinh=ColumnDataSource(data=dict(x=x3,y=y3sinh))
-source3cos=ColumnDataSource(data=dict(x=x3,y=y3cos))
-source3sin=ColumnDataSource(data=dict(x=x3,y=y3sin))
+def eq_cantilever_beam(x):
+    return np.cosh(x)*np.cos(x) + 1.0
+
+
+x_start_fixed_beam, x_start_cant_beam = [[],[]] 
+x_end_fixed_beam, x_end_cant_beam = [[],[]]
+
+# set start and end of intervals surounding a root
+# x_mid approximates the roots (good approximation for ev>3)
+# set some variance to define an interval for the root search via brentq
+for k in range(1,ev_total+1):
+    x_mid_fixed_beam = 0.5*np.pi*(2*k+1)/l
+    x_mid_cant_beam  = 0.5*np.pi*(2*k-1)/l
+    x_start_fixed_beam.append(x_mid_fixed_beam - 0.5)
+    x_start_cant_beam.append(x_mid_cant_beam - 0.5)
+    x_end_fixed_beam.append(x_mid_fixed_beam + 0.5)
+    x_end_cant_beam.append(x_mid_cant_beam + 0.5)
+
+# compute the roots, i.e. eigenvalues
+ev_fixed_beam = np.zeros((ev_total,1))
+ev_cant_beam = np.zeros((ev_total,1))
+for k in range(0,ev_total):
+    ev_fixed_beam[k] = brentq(eq_fixed_beam, x_start_fixed_beam[k], x_end_fixed_beam[k])
+    ev_cant_beam[k]  = brentq(eq_cantilever_beam, x_start_cant_beam[k], x_end_cant_beam[k])
+## end of pre-computation
+
+
+# initialize ColumnDataSources
+source1 = ColumnDataSource(data=dict(x=[0],y=[0]))
+
+source2=ColumnDataSource(data=dict(x=[0],y=[0]))
+source2cosh=ColumnDataSource(data=dict(x=[0],y=[0]))
+source2sinh=ColumnDataSource(data=dict(x=[0],y=[0]))
+source2cos=ColumnDataSource(data=dict(x=[0],y=[0]))
+source2sin=ColumnDataSource(data=dict(x=[0],y=[0]))
+
+source3=ColumnDataSource(data=dict(x=[0],y=[0]))
+source3cosh=ColumnDataSource(data=dict(x=[0],y=[0]))
+source3sinh=ColumnDataSource(data=dict(x=[0],y=[0]))
+source3cos=ColumnDataSource(data=dict(x=[0],y=[0]))
+source3sin=ColumnDataSource(data=dict(x=[0],y=[0]))
+
+    
+# define functions instead of hard-coded solutions
+def beam_simple_supp(ev_num=1, N=200):
+    #print("simple_supp:", ev_num)
+    x1 = np.linspace(0, 1, N)
+    y1=-np.sin(ev_num*np.pi*x1/l)
+    source1.data=dict(x=x1,y=y1)
+    
+def beam_fixed_ends(ev_num=1, N=200):
+    #print("fixed: ", ev_num)
+    x2=np.linspace(0,1,N)
+    ev = ev_fixed_beam[ev_num-1]
+    #print("fixed_val: ", ev)
+    y2=-(1/2*(np.cosh(ev*x2/l)-np.cos(ev*x2/l))-((1/2*(np.cosh(ev)-np.cos(ev)))/(1/2*(np.sinh(ev)-np.sin(ev))))*1/2*(np.sinh(ev*x2/l)-np.sin(ev*x2/l)))
+    y2cosh=-(1/2*(np.cosh(ev*x2/l)))
+    y2sinh=+((1/2*(np.cosh(ev)-np.cos(ev)))/(1/2*(np.sinh(ev)-np.sin(ev))))*1/2*(np.sinh(ev*x2/l))
+    y2cos=-(1/2*(np.cos(ev*x2/l)))
+    y2sin=-((1/2*(np.cosh(ev)-np.cos(ev)))/(1/2*(np.sinh(ev)-np.sin(ev))))*1/2*(np.sin(ev*x2/l))
+    source2.data=dict(x=x2,y=y2)
+    source2cosh.data=dict(x=x2,y=y2cosh)
+    source2sinh.data=dict(x=x2,y=y2sinh)
+    source2cos.data=dict(x=x2,y=y2cos)
+    source2sin.data=dict(x=x2,y=y2sin)
+    
+def beam_cantilever(ev_num=1, N=200):
+    #print("canti: ", ev_num)
+    x3=np.linspace(0,1,N)
+    ev = ev_cant_beam[ev_num-1]
+    #print("cant_val: ", ev)
+    y3=-(1/2*(np.cosh(ev*x3/l)-np.cos(ev*x3/l))-((1/2*(np.cosh(ev)+np.cos(ev)))/(1/2*(np.sinh(ev)+np.sin(ev))))*1/2*(np.sinh(ev*x3/l)-
+                np.sin(ev*x3/l)))
+    y3cosh=-(1/2*(np.cosh(ev*x3/l)))
+    y3sinh=+((1/2*(np.cosh(ev)+np.cos(ev)))/(1/2*(np.sinh(ev)+np.sin(ev))))*1/2*(np.sinh(ev*x3/l))
+    y3cos=-(1/2*(np.cos(ev*x3/l)))
+    y3sin=-((1/2*(np.cosh(ev)+np.cos(ev)))/(1/2*(np.sinh(ev)+np.sin(ev))))*1/2*(np.sin(ev*x3/l))
+    source3.data=dict(x=x3,y=y3)
+    source3cosh.data=dict(x=x3,y=y3cosh)
+    source3sinh.data=dict(x=x3,y=y3sinh)
+    source3cos.data=dict(x=x3,y=y3cos)
+    source3sin.data=dict(x=x3,y=y3sin)
+
+    
+# call functions for initial view
+beam_simple_supp()
+beam_fixed_ends()
+beam_cantilever()
+
+
 
 #Plot 1
 p1 = figure(plot_height=250, plot_width=900,title="Simply supported beam", tools="", x_range=(-0.075,1.075), y_range=(-2.5,2.5))
@@ -116,7 +189,7 @@ fixed_support_right=p2.line(x='x', y='y', source=fixed_right,line_width=2,line_c
 legend2 = Legend(items=[
     ("Eigenvalue Problem: cosh("u"\u03BB)"u"\u00B7cos("u"\u03BB)-1=0 "u"\u279C Solution: w"u"\u1D62("u"\u03BE) = c("u"\u03BB"u"\u1D62"u"\u03BE) - c("u"\u03BB"u"\u1D62) / s("u"\u03BB"u"\u1D62) "u"\u00B7s ("u"\u03BB"u"\u1D62"u"\u03BE)", [eigenmodes_beam2]),
     ("Solution part: "u"\u00BD "u"\u00B7 cosh ("u"\u03BB"u"\u00B7"u"\u03BE)", [eigenmodes_beam2cosh]),
-    ("Solution part: -"u"\u00BD  "u"\u00B7 c("u"\u03BB)/s("u"\u03BB)"u"\u00B7 sinh ("u"\u03BB"u"\u00B7"u"\u03BE)", [eigenmodes_beam2sinh]),
+    ("Solution part: -"u"\u00BD "u"\u00B7 c("u"\u03BB)/s("u"\u03BB)"u"\u00B7 sinh ("u"\u03BB"u"\u00B7"u"\u03BE)", [eigenmodes_beam2sinh]),
     ("Solution part: -"u"\u00BD "u"\u00B7 cos ("u"\u03BB"u"\u00B7"u"\u03BE)", [eigenmodes_beam2cos]),
     ("Solution part: "u"\u00BD "u"\u00B7 c("u"\u03BB)/s("u"\u03BB) "u"\u00B7 sin ("u"\u03BB"u"\u00B7"u"\u03BE)", [eigenmodes_beam2sin]),
 ], location=(0, 0))
@@ -172,131 +245,46 @@ p4.legend.border_line_color = "white"
 
 #Toggle not necessary
 # coffeescript to link toggle with visible property
-#code = '''\
-#object.visible = toggle.active
-#'''
+code = '''\
+object.visible = Toggle.active
+'''
 #I created 3 callbacks with CustomJS so that when the student presses the button he sees the eigenmodes for the beam he chose.
 #My only problem is that the first time one has to press the button twice in order to get the line. Then it works properly. Dont know why??
-#callback1 = CustomJS.from_coffeescript(code=code, args={})
-#toggle1 = Toggle(label="Simply supported beam", button_type="success",callback=callback1)
-#callback1.args = {'toggle': toggle1, 'object':eigenmodes_beam1 }
+#[Matthias]: I could not reproduce the problem, works fine for me.
+callback1 = CustomJS.from_coffeescript(code=code, args={})
+toggle1 = Toggle(label="Simply supported beam", button_type="success",callback=callback1)
+callback1.args = {'toggle': toggle1, 'object':eigenmodes_beam1 }
 
-#callback2 = CustomJS.from_coffeescript(code=code, args={})
-#toggle2 = Toggle(label="Beam fixed at both ends", button_type="success",callback=callback2)
-#callback2.args = {'toggle': toggle2, 'object':eigenmodes_beam2 }
+callback2 = CustomJS.from_coffeescript(code=code, args={})
+toggle2 = Toggle(label="Beam fixed at both ends", button_type="success",callback=callback2)
+callback2.args = {'toggle': toggle2, 'object':eigenmodes_beam2 }
 
-#callback3 = CustomJS.from_coffeescript(code=code, args={})
-#toggle3 = Toggle(label="Cantilever beam", button_type="success",callback=callback3)
-#callback3.args = {'toggle': toggle3, 'object':eigenmodes_beam3 }
+callback3 = CustomJS.from_coffeescript(code=code, args={})
+toggle3 = Toggle(label="Cantilever beam", button_type="success",callback=callback3)
+callback3.args = {'toggle': toggle3, 'object':eigenmodes_beam3 }
 
 ## Create slider widget to choose eigenvalue 1-4
-Eigenvalue_input = Slider(title="Eigenvalue/Eigenmode", value=1, start=1, end=4, step=1)
+Eigenvalue_input = Slider(title="Eigenvalue/Eigenmode", value=1, start=1, end=ev_total, step=1)
+
 
 def update_data(attrname, old, new):
     global radio_button_group
-    eigen_num=Eigenvalue_input.value
-    #simply supported beam
-    x1 = np.linspace(0, 1, N)
-    y1=-np.sin(eigen_num*np.pi*x1/l)
-    source1.data = dict(x=x1, y=y1)
-    #beam fixed at both ends
-    x2=np.linspace(0,1,N)
-    if new==1:
-        y2=-(1/2*(np.cosh(4.73004*x2/l)-np.cos(4.73004*x2/l))-((1/2*(np.cosh(4.73004)-np.cos(4.73004)))/(1/2*(np.sinh(4.73004)-np.sin(4.73004))))*1/2*(np.sinh(4.73004*x2/l)-np.sin(4.73004*x2/l)))
-        y2cosh=-(1/2*(np.cosh(4.73004*x2/l)))
-        y2sinh=+((1/2*(np.cosh(4.73004)-np.cos(4.73004)))/(1/2*(np.sinh(4.73004)-np.sin(4.73004))))*1/2*(np.sinh(4.73004*x2/l))
-        y2cos=-(1/2*(np.cos(4.73004*x2/l)))
-        y2sin=-((1/2*(np.cosh(4.73004)-np.cos(4.73004)))/(1/2*(np.sinh(4.73004)-np.sin(4.73004))))*1/2*(np.sin(4.73004*x2/l))
-        source2.data=dict(x=x2,y=y2)
-        source2cosh.data=dict(x=x2,y=y2cosh)
-        source2sinh.data=dict(x=x2,y=y2sinh)
-        source2cos.data=dict(x=x2,y=y2cos)
-        source2sin.data=dict(x=x2,y=y2sin)
-    elif new==2:
-        y2=-(1/2*(np.cosh(7.853*x2/l)-np.cos(7.853*x2/l))-((1/2*(np.cosh(7.853)-np.cos(7.853)))/(1/2*(np.sinh(7.853)-np.sin(7.853))))*1/2*(np.sinh(7.853*x2/l)-np.sin(7.853*x2/l)))
-        y2cosh=-(1/2*(np.cosh(7.853*x2/l)))
-        y2sinh=+((1/2*(np.cosh(7.853)-np.cos(7.853)))/(1/2*(np.sinh(7.853)-np.sin(7.853))))*1/2*(np.sinh(7.853*x2/l))
-        y2cos=-(1/2*(np.cos(7.853*x2/l)))
-        y2sin=-((1/2*(np.cosh(7.853)-np.cos(7.853)))/(1/2*(np.sinh(7.853)-np.sin(7.853))))*1/2*(np.sin(7.853*x2/l))
-        source2.data=dict(x=x2,y=y2)
-        source2cosh.data=dict(x=x2,y=y2cosh)
-        source2sinh.data=dict(x=x2,y=y2sinh)
-        source2cos.data=dict(x=x2,y=y2cos)
-        source2sin.data=dict(x=x2,y=y2sin)
-    elif new==3:
-        y2=-(1/2*(np.cosh(10.99561*x2/l)-np.cos(10.99561*x2/l))-((1/2*(np.cosh(10.99561)-np.cos(10.99561)))/(1/2*(np.sinh(10.99561)-np.sin(10.99561))))*1/2*(np.sinh(10.99561*x2/l)-np.sin(10.99561*x2/l)))
-        y2cosh=-(1/2*(np.cosh(10.99561*x2/l)))
-        y2sinh=+((1/2*(np.cosh(10.99561)-np.cos(10.99561)))/(1/2*(np.sinh(10.99561)-np.sin(10.99561))))*1/2*(np.sinh(10.99561*x2/l))
-        y2cos=-(1/2*(np.cos(10.99561*x2/l)))
-        y2sin=-((1/2*(np.cosh(10.99561)-np.cos(10.99561)))/(1/2*(np.sinh(10.99561)-np.sin(10.99561))))*1/2*(np.sin(10.99561*x2/l))
-        source2.data=dict(x=x2,y=y2)
-        source2cosh.data=dict(x=x2,y=y2cosh)
-        source2sinh.data=dict(x=x2,y=y2sinh)
-        source2cos.data=dict(x=x2,y=y2cos)
-        source2sin.data=dict(x=x2,y=y2sin)
-    elif new==4:
-        y2=-(1/2*(np.cosh(14.137*x2/l)-np.cos(14.137*x2/l))-((1/2*(np.cosh(14.137)-np.cos(14.137)))/(1/2*(np.sinh(14.137)-np.sin(14.137))))*1/2*(np.sinh(14.137*x2/l)-np.sin(14.137*x2/l)))
-        y2cosh=-(1/2*(np.cosh(14.137*x2/l)))
-        y2sinh=+((1/2*(np.cosh(14.137)-np.cos(14.137)))/(1/2*(np.sinh(14.137)-np.sin(14.137))))*1/2*(np.sinh(14.137*x2/l))
-        y2cos=-(1/2*(np.cos(14.137*x2/l)))
-        y2sin=-((1/2*(np.cosh(14.137)-np.cos(14.137)))/(1/2*(np.sinh(14.137)-np.sin(14.137))))*1/2*(np.sin(14.137*x2/l))
-        source2.data=dict(x=x2,y=y2)
-        source2cosh.data=dict(x=x2,y=y2cosh)
-        source2sinh.data=dict(x=x2,y=y2sinh)
-        source2cos.data=dict(x=x2,y=y2cos)
-        source2sin.data=dict(x=x2,y=y2sin)
-    #cantilever beam
-    x3=np.linspace(0,1,N)
-    if new==1:
-        y3=-(1/2*(np.cosh(1.8751*x3/l)-np.cos(1.8751*x3/l))-((1/2*(np.cosh(1.8751)+np.cos(1.8751)))/(1/2*(np.sinh(1.8751)+np.sin(1.8751))))*1/2*(np.sinh(1.8751*x3/l)-np.sin(1.8751*x3/l)))
-        y3=-(1/2*(np.cosh(1.8751*x3/l)-np.cos(1.8751*x3/l))-((1/2*(np.cosh(1.8751)+np.cos(1.8751)))/(1/2*(np.sinh(1.8751)+np.sin(1.8751))))*1/2*(np.sinh(1.8751*x3/l)-
-            np.sin(1.8751*x3/l)))
-        y3cosh=-(1/2*(np.cosh(1.8751*x2/l)))
-        y3sinh=+((1/2*(np.cosh(1.8751)+np.cos(1.8751)))/(1/2*(np.sinh(1.8751)+np.sin(1.8751))))*1/2*(np.sinh(1.8751*x2/l))
-        y3cos=-(1/2*(np.cos(1.8751*x2/l)))
-        y3sin=-((1/2*(np.cosh(1.8751)+np.cos(1.8751)))/(1/2*(np.sinh(1.8751)+np.sin(1.8751))))*1/2*(np.sin(1.8751*x2/l))
-        source3.data=dict(x=x3,y=y3)
-        source3cosh.data=dict(x=x3,y=y3cosh)
-        source3sinh.data=dict(x=x3,y=y3sinh)
-        source3cos.data=dict(x=x3,y=y3cos)
-        source3sin.data=dict(x=x3,y=y3sin)
-    elif new==2:
-        y3=-(1/2*(np.cosh(4.69409*x3/l)-np.cos(4.69409*x3/l))-((1/2*(np.cosh(4.69409)+np.cos(4.69409)))/(1/2*(np.sinh(4.69409)+np.sin(4.69409))))*1/2*(np.sinh(4.69409*x3/l)-
-            np.sin(4.69409*x3/l)))
-        y3cosh=-(1/2*(np.cosh(4.69409*x2/l)))
-        y3sinh=+((1/2*(np.cosh(4.69409)+np.cos(4.69409)))/(1/2*(np.sinh(4.69409)+np.sin(4.69409))))*1/2*(np.sinh(4.69409*x2/l))
-        y3cos=-(1/2*(np.cos(4.69409*x2/l)))
-        y3sin=-((1/2*(np.cosh(4.69409)+np.cos(4.69409)))/(1/2*(np.sinh(4.69409)+np.sin(4.69409))))*1/2*(np.sin(4.69409*x2/l))
-        source3.data=dict(x=x3,y=y3)
-        source3cosh.data=dict(x=x3,y=y3cosh)
-        source3sinh.data=dict(x=x3,y=y3sinh)
-        source3cos.data=dict(x=x3,y=y3cos)
-        source3sin.data=dict(x=x3,y=y3sin)
-    elif new==3:
-        y3=-(1/2*(np.cosh(7.85476*x3/l)-np.cos(7.85476*x3/l))-((1/2*(np.cosh(7.85476)+np.cos(7.85476)))/(1/2*(np.sinh(7.85476)+np.sin(7.85476))))*1/2*(np.sinh(7.85476*x3/l)-np.sin(7.85476*x3/l)))
-        y3cosh=-(1/2*(np.cosh(7.85476*x2/l)))
-        y3sinh=+((1/2*(np.cosh(7.85476)+np.cos(7.85476)))/(1/2*(np.sinh(7.85476)+np.sin(7.85476))))*1/2*(np.sinh(7.85476*x2/l))
-        y3cos=-(1/2*(np.cos(7.85476*x2/l)))
-        y3sin=-((1/2*(np.cosh(7.85476)+np.cos(7.85476)))/(1/2*(np.sinh(7.85476)+np.sin(7.85476))))*1/2*(np.sin(7.85476*x2/l))
-        source3.data=dict(x=x3,y=y3)
-        source3cosh.data=dict(x=x3,y=y3cosh)
-        source3sinh.data=dict(x=x3,y=y3sinh)
-        source3cos.data=dict(x=x3,y=y3cos)
-        source3sin.data=dict(x=x3,y=y3sin)
-    elif new==4:
-        y3=-(1/2*(np.cosh(10.99554*x3/l)-np.cos(10.99554*x3/l))-((1/2*(np.cosh(10.99554)+np.cos(10.99554)))/(1/2*(np.sinh(10.99554)+np.sin(10.99554))))*1/2*(np.sinh(10.99554*x3/l)-np.sin(10.99554*x3/l)))
-        y3cosh=-(1/2*(np.cosh(10.99554*x2/l)))
-        y3sinh=+((1/2*(np.cosh(10.99554)+np.cos(10.99554)))/(1/2*(np.sinh(10.99554)+np.sin(10.99554))))*1/2*(np.sinh(10.99554*x2/l))
-        y3cos=-(1/2*(np.cos(10.99554*x2/l)))
-        y3sin=-((1/2*(np.cosh(10.99554)+np.cos(10.99554)))/(1/2*(np.sinh(10.99554)+np.sin(10.99554))))*1/2*(np.sin(10.99554*x2/l))
-        source3.data=dict(x=x3,y=y3)
-        source3cosh.data=dict(x=x3,y=y3cosh)
-        source3sinh.data=dict(x=x3,y=y3sinh)
-        source3cos.data=dict(x=x3,y=y3cos)
-        source3sin.data=dict(x=x3,y=y3sin)
+    
+    beam_simple_supp(ev_num=new)
+    beam_fixed_ends(ev_num=new)
+    beam_cantilever(ev_num=new)
+    
     
 Eigenvalue_input.on_change('value',update_data)
 
-curdoc().add_root(row(column(p1,p2,p3),column(Eigenvalue_input,p4)))
+
+# app description
+description_filename = join(dirname(__file__), "description.html")
+description = LatexDiv(text=open(description_filename).read(), render_as_text=False, width=1200)
+
+#curdoc().add_root(row(column(p1,p2,p3),column(Eigenvalue_input,p4)))
+def_scroll = column(Eigenvalue_input, css_classes=['scrollable']) # also defined in index.html file
+curdoc().add_root(column(description, row(column(p1,p2,p3),def_scroll)))
+#curdoc().add_root(column(description, row(column(p1,p2,p3),column(Eigenvalue_input,p4))))
 curdoc().title = split(dirname(__file__))[-1].replace('_',' ').replace('-',' ')  # get path of parent directory and only use the name of the Parent Directory for the tab name. Replace underscores '_' and minuses '-' with blanks ' '
 
