dca098ec890d107daf5d201dac5cc91a55163c10
qaumann <qaumann@users.noreply.github.com>
2018-06-27 20:52:44 +0200

    Adding new Latex slider

---
+++ shared/latex_slider.py
@@ -0,0 +1,6 @@
+from bokeh.models.widgets import Slider
+
+class LatexSlider(Slider):
+    __javascript__ = ["https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.js"]
+    __css__ = ["https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css"]
+    __implementation__ = "latex_slider.ts"
