59cc7d3edd4b5245ff1a387342cad7b3f8a8fc4e
qaumann <qaumann@users.noreply.github.com>
2018-07-10 18:15:02 +0200

    New autorun script

---
+++ server_autorun.cmd
@@ -0,0 +1,3 @@
+start powershell -ExecutionPolicy Bypass .\bokehrunner.ps1
+start powershell -ExecutionPolicy Bypass .\bokehrunner_math.ps1
+start powershell -ExecutionPolicy Bypass .\bokehrunner_testing.ps1
--- server_autorun.ps1
+++
@@ -1,3 +0,0 @@
-Start-Process powershell -ArgumentList "-command .\bokehrunner.ps1"
-Start-Process powershell -ArgumentList "-command .\bokehrunner_math.ps1"
-Start-Process powershell -ArgumentList "-command .\bokehrunner_testing.ps1"
