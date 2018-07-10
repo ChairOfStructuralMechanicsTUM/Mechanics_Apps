a1cb68930b48c9b787eddb2691e67c2c3712c2f4
qaumann <qaumann@users.noreply.github.com>
2018-07-10 18:46:26 +0200

    minor

--- server_autorun.cmd
+++ server_autorun.cmd
@@ -1,3 +1,4 @@
+cd C:\Path\to\Bokeh\apps
 start powershell -ExecutionPolicy Bypass .\bokehrunner.ps1
 start powershell -ExecutionPolicy Bypass .\bokehrunner_math.ps1
 start powershell -ExecutionPolicy Bypass .\bokehrunner_testing.ps1
