c942c01894534843d75730a270f72cbc84ecb86d
Sascha Kubish <38219974+ga96wec@users.noreply.github.com>
2018-08-16 11:49:01 +0200

    betty displacements w21 done
    tasks left:
    -lock y-movement of triangle
    -solution for vertical displacements in middle part of frame

--- Maxwell_Reciprocity/main.py
+++ Maxwell_Reciprocity/main.py
@@ -528,7 +528,6 @@ def create_shift(f):
     '''Changes the values of the arrows that display how much of
     a shift has occurred'''
     
-
     if (f.get_mag() == 0):
         if (f.get_param() == 0):
             f.e_s.data = dict(xS= [], xE= [], yS= [], yE=[], lW = [] )
@@ -537,9 +536,9 @@ def create_shift(f):
         localDouble1 = compute_shift(paramInt1,paramInt1, f.get_mag())
         
         if (f.name == "F"u"\u2081"):
-            names = "(w"u"\u2081"u"\u2081)"
+            names = " w"u"\u2081"u"\u2081"
         elif (f.name == "F"u"\u2082"):
-            names = "(w"u"\u2082"u"\u2082)"
+            names = " w"u"\u2082"u"\u2082"
 
  
         d2 = 0
@@ -622,7 +621,7 @@ def create_wdline(f):
 def calc_betty_displacements12(f):
     ParamInt = f.get_param()
     i = f.get_mag()
-    names12 = "w"u"\u2081"u"\u2082" 
+    names12 = " w"u"\u2081"u"\u2082" 
     
     if ParamInt < 30:
         x=0.1
@@ -744,61 +743,62 @@ def calc_betty_displacements12(f):
 
 #EDIT            
 def calc_betty_displacements21(f):
-    ParamInt = f.get_param()
+    ParamInt = f1.get_param()
+    #print ParamInt
     i = f.get_mag()
-    names12 = "w"u"\u2082"u"\u2081" 
+    names21 = " w"u"\u2082"u"\u2081" 
     
     if ParamInt < 30:
         x=0.1
-        y=0.85
+        y=-0.16
         x_l=0.1
-        y_l=0.8
+        y_l=-0.15
         if ParamInt < 8:
             dx = ParamInt
-            x_betty12 = (f1.pts.data["x"][0]) + (dx/7.0)*((f1.pts.data["x"][1])-(f1.pts.data["x"][0]))
+            x_betty21 = (f2.pts.data["x"][0]) + (dx/7.0)*((f2.pts.data["x"][1])-(f2.pts.data["x"][0]))
             #print f1.pts.data
             #print ParamInt
             #print x_betty21
-            f.w12.data = dict(xS= [x], xE= [x],
-                yS= [y], yE=[y], name = [names12] )
-            f.w12_11.data = dict(xS= [x], xE= [x_betty12],
+            f.w21.data = dict(xS= [x], xE= [x],
+                yS= [y], yE=[y], name = [names21] )
+            f.w21_11.data = dict(xS= [x], xE= [x_betty21],
                 yS= [y_l], yE=[y_l] )    
-            f.w12_12.data = dict(xS= [x_betty12], xE= [x],
+            f.w21_12.data = dict(xS= [x_betty21], xE= [x],
                 yS= [y_l], yE=[y_l] )
-            f.wdline12.data = dict(x1 = [ x_betty12, x_betty12 ]  , x2 = [ x_l, x_l ] ,
+            f.wdline21.data = dict(x1 = [ x_betty21, x_betty21 ]  , x2 = [ x_l, x_l ] ,
             y1 = [y_l ,ParamInt / 30.0 * 0.5 + 0.1] , y2 = [ y_l, ParamInt / 30.0 * 0.5 + 0.1 ] )
         if ParamInt >7 and ParamInt <15:
             dx = ParamInt-8
-            x_betty12 = (f1.pts.data["x"][1]) + (dx/7.0)*((f1.pts.data["x"][2])-(f1.pts.data["x"][1]))
-            f.w12.data = dict(xS= [x], xE= [x],
-                yS= [y], yE=[y], name = [names12] )
-            f.w12_11.data = dict(xS= [x], xE= [x_betty12],
+            x_betty21 = (f2.pts.data["x"][1]) + (dx/7.0)*((f2.pts.data["x"][2])-(f2.pts.data["x"][1]))
+            f.w21.data = dict(xS= [x], xE= [x],
+                yS= [y], yE=[y], name = [names21] )
+            f.w21_11.data = dict(xS= [x], xE= [x_betty21],
                 yS= [y_l], yE=[y_l] )    
-            f.w12_12.data = dict(xS= [x_betty12], xE= [x],
+            f.w21_12.data = dict(xS= [x_betty21], xE= [x],
                 yS= [y_l], yE=[y_l] )
-            f.wdline12.data = dict(x1 = [ x_betty12, x_betty12 ]  , x2 = [ x_l, x_l ] ,
+            f.wdline21.data = dict(x1 = [ x_betty21, x_betty21 ]  , x2 = [ x_l, x_l ] ,
             y1 = [y_l ,ParamInt / 30.0 * 0.5 + 0.1] , y2 = [ y_l, ParamInt / 30.0 * 0.5 + 0.1 ] )
         if ParamInt >14 and ParamInt <22:
             dx = ParamInt-15
-            x_betty12 = (f1.pts.data["x"][2]) + (dx/7.0)*((f1.pts.data["x"][3])-(f1.pts.data["x"][2]))
-            f.w12.data = dict(xS= [x], xE= [x],
-                yS= [y], yE=[y], name = [names12] )
-            f.w12_11.data = dict(xS= [x], xE= [x_betty12],
+            x_betty21 = (f2.pts.data["x"][2]) + (dx/7.0)*((f2.pts.data["x"][3])-(f2.pts.data["x"][2]))
+            f.w21.data = dict(xS= [x], xE= [x],
+                yS= [y], yE=[y], name = [names21] )
+            f.w21_11.data = dict(xS= [x], xE= [x_betty21],
                 yS= [y_l], yE=[y_l] )    
-            f.w12_12.data = dict(xS= [x_betty12], xE= [x],
+            f.w21_12.data = dict(xS= [x_betty21], xE= [x],
                 yS= [y_l], yE=[y_l] )
-            f.wdline12.data = dict(x1 = [ x_betty12, x_betty12 ]  , x2 = [ x_l, x_l ] ,
+            f.wdline21.data = dict(x1 = [ x_betty21, x_betty21 ]  , x2 = [ x_l, x_l ] ,
             y1 = [y_l ,ParamInt / 30.0 * 0.5 + 0.1] , y2 = [ y_l, ParamInt / 30.0 * 0.5 + 0.1 ] )
         if ParamInt >21:
             dx = ParamInt-22
-            x_betty12 = (f1.pts.data["x"][3]) + (dx/8.0)*((f1.pts.data["x"][4])-(f1.pts.data["x"][3]))
-            f.w12.data = dict(xS= [x], xE= [x],
-                yS= [y], yE=[y], name = [names12] )
-            f.w12_11.data = dict(xS= [x], xE= [x_betty12],
+            x_betty12 = (f2.pts.data["x"][3]) + (dx/8.0)*((f2.pts.data["x"][4])-(f2.pts.data["x"][3]))
+            f.w21.data = dict(xS= [x], xE= [x],
+                yS= [y], yE=[y], name = [names21] )
+            f.w21_11.data = dict(xS= [x], xE= [x_betty21],
                 yS= [y_l], yE=[y_l] )    
-            f.w12_12.data = dict(xS= [x_betty12], xE= [x],
+            f.w21_12.data = dict(xS= [x_betty21], xE= [x],
                 yS= [y_l], yE=[y_l] )
-            f.wdline12.data = dict(x1 = [ x_betty12, x_betty12 ]  , x2 = [ x_l, x_l ] ,
+            f.wdline21.data = dict(x1 = [ x_betty21, x_betty21 ]  , x2 = [ x_l, x_l ] ,
             y1 = [y_l ,ParamInt / 30.0 * 0.5 + 0.1] , y2 = [ y_l, ParamInt / 30.0 * 0.5 + 0.1 ] )
     
     elif ((30 <= ParamInt) & (ParamInt <= 70)):
@@ -806,63 +806,63 @@ def calc_betty_displacements21(f):
         #    dx = ParamInt-37
     
         #    y_betty12 = (f1.pts.data["y"][4]) + (dx/7.0)*((f1.pts.data["y"][5])-(f1.pts.data["y"][4]))
-            f.w12.data = dict(xS= [], xE= [],
-                yS= [], yE=[], name = [names12] )
-            f.w12_12.data = dict(xS= [], xE= [],
+            f.w21.data = dict(xS= [], xE= [],
+                yS= [], yE=[], name = [names21] )
+            f.w21_12.data = dict(xS= [], xE= [],
                 yS= [], yE=[] )
-            f.w12_11.data = dict(xS= [], xE= [],
+            f.w21_11.data = dict(xS= [], xE= [],
                 yS= [], yE=[] )
-            f.wdline12.data = dict(x1 = []  , x2 = [] ,
+            f.wdline21.data = dict(x1 = []  , x2 = [] ,
             y1 = [] , y2 = [] )
       
     elif ParamInt > 70:
         x=0.8
-        y=0.85
+        y=-0.16
         x_l=0.8
-        y_l=0.8
+        y_l=-0.15
         if ParamInt >70 and ParamInt <79:
             dx = ParamInt-71
-            x_betty12 = (f1.pts.data["x"][8]) + (dx/7.0)*((f1.pts.data["x"][9])-(f1.pts.data["x"][8]))
-            f.w12.data = dict(xS= [x], xE= [x],
-                yS= [y], yE=[y], name = [names12] )
-            f.w12_11.data = dict(xS= [x], xE= [x_betty12],
+            x_betty21 = (f2.pts.data["x"][8]) + (dx/7.0)*((f2.pts.data["x"][9])-(f2.pts.data["x"][8]))
+            f.w21.data = dict(xS= [x], xE= [x],
+                yS= [y], yE=[y], name = [names21] )
+            f.w21_11.data = dict(xS= [x], xE= [x_betty21],
                 yS= [y_l], yE=[y_l] )    
-            f.w12_12.data = dict(xS= [x_betty12], xE= [x],
+            f.w21_12.data = dict(xS= [x_betty21], xE= [x],
                 yS= [y_l], yE=[y_l] )
-            f.wdline12.data = dict(x1 = [ x_betty12, x_betty12 ]  , x2 = [ x_l, x_l ] ,
+            f.wdline21.data = dict(x1 = [ x_betty21, x_betty21 ]  , x2 = [ x_l, x_l ] ,
             y1 = [ y_l , 0.6 - (ParamInt - 70) / 30.0 * 0.5 ] , y2 = [ y_l, 0.6 - (ParamInt - 70) / 30.0 * 0.5 ] )
         if ParamInt >78 and ParamInt <86:
             dx = ParamInt-79
-            x_betty12 = (f1.pts.data["x"][9]) + (dx/6.0)*((f1.pts.data["x"][10])-(f1.pts.data["x"][9]))
-            f.w12.data = dict(xS= [x], xE= [x],
-                yS= [y], yE=[y], name = [names12] )
-            f.w12_11.data = dict(xS= [x], xE= [x_betty12],
+            x_betty21 = (f2.pts.data["x"][9]) + (dx/6.0)*((f2.pts.data["x"][10])-(f2.pts.data["x"][9]))
+            f.w21.data = dict(xS= [x], xE= [x],
+                yS= [y], yE=[y], name = [names21] )
+            f.w21_11.data = dict(xS= [x], xE= [x_betty21],
                 yS= [y_l], yE=[y_l] )    
-            f.w12_12.data = dict(xS= [x_betty12], xE= [x],
+            f.w21_12.data = dict(xS= [x_betty21], xE= [x],
                 yS= [y_l], yE=[y_l] )
-            f.wdline12.data = dict(x1 = [ x_betty12, x_betty12 ]  , x2 = [ x_l, x_l ] ,
+            f.wdline21.data = dict(x1 = [ x_betty21, x_betty21 ]  , x2 = [ x_l, x_l ] ,
             y1 = [ y_l , 0.6 - (ParamInt - 70) / 30.0 * 0.5 ] , y2 = [ y_l, 0.6 - (ParamInt - 70) / 30.0 * 0.5 ] )
         if ParamInt >85 and ParamInt <93:
             dx = ParamInt-86
-            x_betty12 = (f1.pts.data["x"][10]) + (dx/6.0)*((f1.pts.data["x"][11])-(f1.pts.data["x"][10]))
-            f.w12.data = dict(xS= [x], xE= [x],
-                yS= [y], yE=[y], name = [names12] )
-            f.w12_11.data = dict(xS= [x], xE= [x_betty12],
+            x_betty21 = (f2.pts.data["x"][10]) + (dx/6.0)*((f2.pts.data["x"][11])-(f2.pts.data["x"][10]))
+            f.w21.data = dict(xS= [x], xE= [x],
+                yS= [y], yE=[y], name = [names21] )
+            f.w21_11.data = dict(xS= [x], xE= [x_betty21],
                 yS= [y_l], yE=[y_l] )    
-            f.w12_12.data = dict(xS= [x_betty12], xE= [x],
+            f.w21_12.data = dict(xS= [x_betty21], xE= [x],
                 yS= [y_l], yE=[y_l] )
-            f.wdline12.data = dict(x1 = [ x_betty12, x_betty12 ]  , x2 = [ x_l, x_l ] ,
+            f.wdline21.data = dict(x1 = [ x_betty21, x_betty21 ]  , x2 = [ x_l, x_l ] ,
             y1 = [ y_l , 0.6 - (ParamInt - 70) / 30.0 * 0.5 ] , y2 = [ y_l, 0.6 - (ParamInt - 70) / 30.0 * 0.5 ] )
         if ParamInt >92:
             dx = ParamInt-93
-            x_betty12 = (f1.pts.data["x"][11]) + dx/8.0*((f1.pts.data["x"][12])-(f1.pts.data["x"][11]))
-            f.w12.data = dict(xS= [x], xE= [x],
-                yS= [y], yE=[y], name = [names12] )
-            f.w12_11.data = dict(xS= [x], xE= [x_betty12],
+            x_betty21 = (f2.pts.data["x"][11]) + dx/8.0*((f2.pts.data["x"][12])-(f2.pts.data["x"][11]))
+            f.w21.data = dict(xS= [x], xE= [x],
+                yS= [y], yE=[y], name = [names21] )
+            f.w21_11.data = dict(xS= [x], xE= [x_betty21],
                 yS= [y_l], yE=[y_l] )    
-            f.w12_12.data = dict(xS= [x_betty12], xE= [x],
+            f.w21_12.data = dict(xS= [x_betty21], xE= [x],
                 yS= [y_l], yE=[y_l] )
-            f.wdline12.data = dict(x1 = [ x_betty12, x_betty12 ]  , x2 = [ x_l, x_l ] ,
+            f.wdline21.data = dict(x1 = [ x_betty21, x_betty21 ]  , x2 = [ x_l, x_l ] ,
             y1 = [ y_l , 0.6 - (ParamInt - 70) / 30.0 * 0.5 ] , y2 = [ y_l, 0.6 - (ParamInt - 70) / 30.0 * 0.5 ] )         
 
 
@@ -1124,10 +1124,10 @@ e2_arrow_glyph = Arrow(end=OpenHead(line_color=f2color,line_width= 3, size=6,lin
     x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= 4, source=f2.e_s,line_color=f2color,line_alpha = 0.5)
 
 #F1 w11 arrow
-w11_arrow_glyph = Arrow(end=OpenHead(line_color=f1color,line_width= 3, size=6,line_alpha = 0.5),
-    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= 4, source=f1.w1,line_color=f1color,line_alpha = 0.4)
-w12_arrow_glyph = Arrow(end=OpenHead(line_color=f1color,line_width= 3, size=6,line_alpha = 0.5),
-    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= 4, source=f1.w2,line_color=f1color,line_alpha = 0.4)
+w11_arrow_glyph = Arrow(end=OpenHead(line_color=f1color,line_width= 3, size=6,line_alpha = 0.3),
+    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= 4, source=f1.w1,line_color=f1color,line_alpha = 0.3)
+w12_arrow_glyph = Arrow(end=OpenHead(line_color=f1color,line_width= 3, size=6,line_alpha = 0.3),
+    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= 4, source=f1.w2,line_color=f1color,line_alpha = 0.3)
 
 #EDIT
 #F1 w12 arrow
@@ -1137,17 +1137,17 @@ w12_12_arrow_glyph = Arrow(end=OpenHead(line_color=f1color,line_width= 3, size=6
     x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= 4, source=f2.w12_12,line_color=f1color,line_alpha = 0.5)
 
 #F2 w22 arrow
-w21_arrow_glyph = Arrow(end=OpenHead(line_color=f2color,line_width= 3, size=6,line_alpha = 0.0),
-    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= 4, source=f2.w1,line_color=f2color,line_alpha = 0.0)
-w22_arrow_glyph = Arrow(end=OpenHead(line_color=f2color,line_width= 3, size=6,line_alpha = 0.0),
-    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= 4, source=f2.w2,line_color=f2color,line_alpha = 0.0)
+w21_arrow_glyph = Arrow(end=OpenHead(line_color=f2color,line_width= 3, size=6,line_alpha = 0.3),
+    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= 4, source=f2.w1,line_color=f2color,line_alpha = 0.3)
+w22_arrow_glyph = Arrow(end=OpenHead(line_color=f2color,line_width= 3, size=6,line_alpha = 0.3),
+    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= 4, source=f2.w2,line_color=f2color,line_alpha = 0.3)
 
 #EDIT
 #F2 w21 arrow
-w21_11_arrow_glyph = Arrow(end=OpenHead(line_color=f1color,line_width= 3, size=6,line_alpha = 0.5),
-    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= 4, source=f2.w1,line_color=f2color,line_alpha = 0.4)
-w22_12_arrow_glyph = Arrow(end=OpenHead(line_color=f1color,line_width= 3, size=6,line_alpha = 0.5),
-    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= 4, source=f2.w2,line_color=f2color,line_alpha = 0.4)
+w21_11_arrow_glyph = Arrow(end=OpenHead(line_color=f2color,line_width= 3, size=6,line_alpha = 0.5),
+    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= 4, source=f2.w21_11,line_color=f2color,line_alpha = 0.5)
+w22_12_arrow_glyph = Arrow(end=OpenHead(line_color=f2color,line_width= 3, size=6,line_alpha = 0.5),
+    x_start='xS', y_start='yS', x_end='xE', y_end='yE',line_width= 4, source=f2.w21_12,line_color=f2color,line_alpha = 0.5)
 
 ########
 
