5a6c25a9962aa7b1e7492c0270ecab96122bf38d
Sascha Kubisch <38219974+ga96wec@users.noreply.github.com>
2018-10-22 19:15:22 +0200

    Changes 22.10.
    - divide plots for shear force and moment
    - let supports disappear if cantilever
    - loadvalue in [-1;1]
    - rescale plots

--- Balken_AD/main.py
+++ Balken_AD/main.py
@@ -11,14 +11,14 @@ import numpy as np
 from os.path import dirname, join, split
 
 #Global Beam Properties:
-resol = 100
+resol = 100             #EDIT: resolution?
 x0 = 0                  #starting value of beam
 xf = 10                 #ending value of beam
 E  = 500.0e1            #modulus of elasticity
 I  = 30                 #moment of inertia
-length  = xf-x0              #length of beam
-p_mag = 100.0           #initialize the p force
-p_magi = 100.0
+length  = xf-x0         #length of beam
+p_mag = 0.5               #initialize the p force
+p_magi = 0.5
 p_loci = xf/2
 f2_loci = xf
 lthi = 2
@@ -47,9 +47,9 @@ support_source2 = ColumnDataSource(data=dict(sp2=[], x=[] , y=[]))
 quad_source = ColumnDataSource(data=dict(top= [], bottom= [],left = [], right =[]))
 segment_source = ColumnDataSource(data=dict(x0= [], y0= [],x1 = [], y1 =[]))
 #Sliders:
-p_loc_slide= Slider(title="Load Position",value= p_loci,start = x0, end = xf, step = 1/resol)
-p_mag_slide = Slider(title="Load Amplitude", value=p_mag, start=-2*p_mag, end=2*p_mag, step=1)
-f2_loc_slide = Slider(title="Support Position",value=f2_loci,start = x0, end = xf, step = 1/resol)
+p_loc_slide= Slider(title="Load Position",value= p_loci,start = x0, end = xf, step = 1)
+p_mag_slide = Slider(title="Load Amplitude", value=p_mag, start=-2*p_mag, end=2*p_mag, step=.1)
+f2_loc_slide = Slider(title="Support Position",value=f2_loci,start = x0, end = xf, step = 1)
 lth_slide = Slider(title="Beam-Height",value=lthi ,start = 2, end = 20, step = 1)
 
 #FUNCTION: Calculate Force at Support 1
@@ -135,6 +135,10 @@ def Fun_Cantilever():
     x1seg = np.ones(clines) * right
     y1seg = np.linspace(bottom+0.2,top,clines)
     segment_source.data = dict(x0= xseg, y0= yseg,x1 = x1seg, y1 =y1seg)
+    ###
+    support_source2.data = dict(sp2=[], x = [] , y = [])
+    support_source1.data = dict(sp1=[], x = [] , y = [])
+
 #FUNCTION: Update:
 def Fun_Update(attrname, old, new):
     #values used by both cases:
@@ -152,10 +156,10 @@ def Fun_Update(attrname, old, new):
         Fun_Cantilever()
 
         if (p_mag<0):
-            p_arrow_source.data = dict(xS= [p_coord], xE= [p_coord], yS= [1-(p_mag/200.0)], yE=[1], lW = [2] )
+            p_arrow_source.data = dict(xS= [p_coord], xE= [p_coord], yS= [1-(p_mag/1.0)], yE=[1], lW = [2] )
             labels_source.data = dict(x = [p_coord] , y = [1],name = ['F'])
         else:
-            p_arrow_source.data = dict(xS= [p_coord], xE= [p_coord], yS= [-1-(p_mag/200.0)], yE=[-1], lW = [2] )
+            p_arrow_source.data = dict(xS= [p_coord], xE= [p_coord], yS= [-1-(p_mag/1.0)], yE=[-1], lW = [2] )
             labels_source.data = dict(x = [p_coord] , y = [-1],name = ['F'])
 
         #cantilever moment calculation:
@@ -218,38 +222,38 @@ def Fun_Update(attrname, old, new):
 
         #p_arrow and labels:
         if (p_mag<0):
-            p_arrow_source.data = dict(xS= [p_coord], xE= [p_coord], yS= [1-(p_mag/200.0)], yE=[1], lW = [2] )
+            p_arrow_source.data = dict(xS= [p_coord], xE= [p_coord], yS= [1-(p_mag/1.0)], yE=[1], lW = [2] )
             labels_source.data = dict(x = [p_coord,0,f2_coord-0.2] , y = [1,move_tri,move_tri],name = ['F','A','B'])
             support_source2.data = dict(sp2=[support2], x = [f2_coord-0.33] , y = [-0.1])
-
+            support_source1.data = dict(sp1=[support1], x= [-0.325], y= [-0.1])
         else:
-            p_arrow_source.data = dict(xS= [p_coord], xE= [p_coord], yS= [-1-(p_mag/200.0)], yE=[-1], lW = [2] )
+            p_arrow_source.data = dict(xS= [p_coord], xE= [p_coord], yS= [-1-(p_mag/1.0)], yE=[-1], lW = [2] )
             labels_source.data = dict(x = [p_coord,-0.2,f2_coord-0.2] , y = [-1,move_tri,move_tri],name = ['F','A','B'])
             support_source2.data = dict(sp2=[support2], x = [f2_coord-0.33] , y = [-0.1])
-
+            support_source1.data = dict(sp1=[support1], x= [-0.325], y= [-0.1])
         #f1_arrow:
         #if (f1_mag==0):
             #f1_arrow_source.data = dict(xS=[], xE=[], yS=[], yE=[], lW = [])
         if (f1_mag<=-p_magi):
-            f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [1-(f1_mag/200.0)], yE=[0.8], lW = [2])
+            f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [1-(f1_mag/1)], yE=[0.8], lW = [2])
         elif ( f1_mag>-p_magi ) & ( f1_mag<=0 ):
-            f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [1-(f1_mag/200.0)], yE=[0.8], lW = [2])
+            f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [1-(f1_mag/1)], yE=[0.8], lW = [2])
         elif (f1_mag > 0) & ( f1_mag < p_magi ):
-            f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [-1-(f1_mag/200.0)], yE=[-0.8], lW = [2] )
+            f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [-1-(f1_mag/1)], yE=[-0.8], lW = [2] )
         else:
-            f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [-1-(f1_mag/200.0)], yE=[-0.8], lW = [2] )
+            f1_arrow_source.data = dict(xS= [0], xE= [0], yS= [-1-(f1_mag/1)], yE=[-0.8], lW = [2] )
 
         #f2_arrow:
         #if (f2_mag==0):
         #    f2_arrow_source.data = dict(xS=[], xE=[], yS=[], yE=[])
         if (f2_mag<=-p_magi):
-            f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [1-(f2_mag/200.0)], yE=[0.8], lW = [2])
+            f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [1-(f2_mag/1)], yE=[0.8], lW = [2])
         elif (f2_mag > -p_magi) & (f2_mag <= 0.0):
-            f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [1-(f2_mag/200.0)], yE=[0.8], lW = [2])
+            f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [1-(f2_mag/1)], yE=[0.8], lW = [2])
         elif (f2_mag > 0) & ( f2_mag < p_magi ):
-            f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [-1-(f2_mag/200.0)], yE=[-0.8], lW = [2])
+            f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [-1-(f2_mag/1)], yE=[-0.8], lW = [2])
         else:
-            f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [-1-(f2_mag/200.0)], yE=[-0.8], lW =[2])
+            f2_arrow_source.data = dict(xS= [f2_coord], xE= [f2_coord], yS= [-1-(f2_mag/1)], yE=[-0.8], lW =[2])
 
 #initial function:
 def initial():
@@ -282,16 +286,13 @@ labels = LabelSet(x='x', y='y', text='name', level='glyph',
               x_offset=5, y_offset=-30, source=labels_source, render_mode='canvas')
 
 
-###Plot with moment and shear:
-y_range0 = -600
+###Plot1 with moment:
+y_range0 = -10
 y_range1 = -y_range0
-plot1 = Figure(title="Bending Moment, Shear Force", x_range=(x0-.5,xf+.5), y_range=(y_range0,y_range1), height = 200, logo=None)
+plot1 = Figure(title="Bending Moment", x_range=(x0-.5,xf+.5), y_range=(y_range0,y_range1), height = 200, logo=None)
 
 #Insert TUM-COLOUR
 plot1.line(x='x', y='y', source=mom_source, color='blue',line_width=5)
-
-#Insert TUM-COLOUR
-plot1.line(x='x', y='y', source=shear_source, color='red',line_width=5)
 
 plot1.line(x= [x0-1,xf+1], y = [0, 0 ], color = 'black', line_width =2 ,line_alpha = 0.4, line_dash=[1])
 plot1.line(x= [xf/2,xf/2], y = [y_range0,y_range1], color = 'black', line_width =2 ,line_alpha = 0.4, line_dash=[1])
@@ -306,9 +307,37 @@ plot1.title.text_font_size="13pt"
 plot1.square([0.0],[0.0],size=0,fill_color='blue',fill_alpha=0.5,legend="Bending Moment")
 
 #Insert TUM-COLOUR
-plot1.square([0.0],[0.0],size=0,fill_color='red',fill_alpha=0.5,legend="Shear Force")
+#plot1.square([0.0],[0.0],size=0,fill_color='red',fill_alpha=0.5,legend="Shear Force")
 
 plot1.legend.location = 'top_right'
+
+
+###Plot2 with shear:
+y_range0 = -10
+y_range1 = -y_range0
+plot2 = Figure(title="Shear Force", x_range=(x0-.5,xf+.5), y_range=(y_range0,y_range1), height = 200, logo=None)
+
+#Insert TUM-COLOUR
+plot2.line(x='x', y='y', source=shear_source, color='red',line_width=5)
+
+plot2.line(x= [x0-1,xf+1], y = [0, 0 ], color = 'black', line_width =2 ,line_alpha = 0.4, line_dash=[1])
+plot2.line(x= [xf/2,xf/2], y = [y_range0,y_range1], color = 'black', line_width =2 ,line_alpha = 0.4, line_dash=[1])
+plot2.axis.visible = False
+plot2.outline_line_width = 2
+#plot.outline_line_alpha = 0.3
+plot2.outline_line_color = "Black"
+plot2.title.text_font_size="13pt"
+# dummy glyphs for the legend entries
+
+#Insert TUM-COLOUR
+#plot2.square([0.0],[0.0],size=0,fill_color='blue',fill_alpha=0.5,legend="Bending Moment")
+
+#Insert TUM-COLOUR
+plot2.square([0.0],[0.0],size=0,fill_color='red',fill_alpha=0.5,legend="Shear Force")
+
+plot2.legend.location = 'top_right'
+
+
 ###arrow plotting:
 #P arrow:
 p_arrow_glyph = Arrow(end=OpenHead(line_color="#0065BD",line_width= 2, size=5),
@@ -345,5 +374,5 @@ button.on_click(initial)
 #main:
 initial()
 
-curdoc().add_root( row( column(Spacer(height=200,width=50), p_loc_slide, p_mag_slide, f2_loc_slide, widgetbox(button)),  column(plot,plot1 ) ) )
+curdoc().add_root( row( column(Spacer(height=200,width=50), p_loc_slide, p_mag_slide, f2_loc_slide, widgetbox(button)),  column(plot,plot2,plot1 ) ) )
 curdoc().title = split(dirname(__file__))[-1].replace('_',' ').replace('-',' ')  # get path of parent directory and only use the name of the Parent Directory for the tab name. Replace underscores '_' and minuses '-' with blanks ' '
