40dabb841be771e21e44575528833c99d9ae3bee
Matthias <39389064+m3bert@users.noreply.github.com>
2019-02-03 14:42:44 +0100

    tidied code

--- Rolling_test/main.py
+++ Rolling_test/main.py
@@ -7,44 +7,44 @@ from os.path import dirname, split
 import numpy as np
 
 # create variables
-maxR=4.0
-g=9.81
-alpha=radians(20)
+maxR  = 4.0
+g     = 9.81
+alpha = radians(20)
 # variables created to avoid repeated calculations
 # (speeds up calculations)
-SIN=sin(alpha)
-COS=cos(alpha)
-rampLength=50
-offset=-rampLength*COS
-t=0.0
-H = rampLength*SIN
-SphereXLines=[np.array([]),np.array([])]
-SphereYLines=np.array([])
+SIN        = sin(alpha)
+COS        = cos(alpha)
+rampLength = 50
+offset     = -rampLength*COS
+t          = 0.0
+H          = rampLength*SIN
 
-#Active = False
+SphereXLines = [np.array([]),np.array([])]
+SphereYLines = np.array([])
 
 # create ColumnDataSources
-fig1_data = ColumnDataSource(data = dict(x=[],y=[],w=[],c=[],a=[]))
-fig1_lines_data = ColumnDataSource(data = dict(x=[],y=[]))
-fig2_data = ColumnDataSource(data = dict(x=[],y=[],w=[],c=[],a=[]))
-fig2_lines_data = ColumnDataSource(data = dict(x=[],y=[]))
-fig3_data = ColumnDataSource(data = dict(x=[],y=[],w=[],c=[],a=[]))
-fig3_lines_data = ColumnDataSource(data = dict(x=[],y=[]))
-ramp_source = ColumnDataSource(data = dict(x=[offset,0],y=[H,0]))
+fig1_data         = ColumnDataSource(data = dict(x=[],y=[],w=[],c=[],a=[]))
+fig1_lines_data   = ColumnDataSource(data = dict(x=[],y=[]))
+fig2_data         = ColumnDataSource(data = dict(x=[],y=[],w=[],c=[],a=[]))
+fig2_lines_data   = ColumnDataSource(data = dict(x=[],y=[]))
+fig3_data         = ColumnDataSource(data = dict(x=[],y=[],w=[],c=[],a=[]))
+fig3_lines_data   = ColumnDataSource(data = dict(x=[],y=[]))
+ramp_source       = ColumnDataSource(data = dict(x=[offset,0],y=[H,0]))
 AngleMarkerSource = ColumnDataSource(data = dict(x=[],y=[]))
-AlphaPos = ColumnDataSource(data = dict(x=[],y=[],t=[]))
+AlphaPos          = ColumnDataSource(data = dict(x=[],y=[],t=[]))
 
 
 # global variables
-glob_callback_id = ColumnDataSource(data = dict(callback_id = [None]))
+glob_callback_id  = ColumnDataSource(data = dict(callback_id = [None]))
 glob_SphereXLines = ColumnDataSource(data = dict(SphereXLines = [SphereXLines]))
 glob_SphereYLines = ColumnDataSource(data = dict(SphereYLines = [SphereYLines]))
-glob_offset = ColumnDataSource(data = dict(offset = [offset]))
-glob_SIN = ColumnDataSource(data = dict(SIN = [SIN]))
-glob_COS = ColumnDataSource(data = dict(COS = [COS]))
-glob_g = ColumnDataSource(data = dict(g = [g]))
-glob_alpha = ColumnDataSource(data = dict(alpha = [alpha]))
-glob_t = ColumnDataSource(data = dict(t = [t]))
+# following ColumnDataSources could be exchanged by a single dict
+glob_offset       = ColumnDataSource(data = dict(offset = [offset]))
+glob_SIN          = ColumnDataSource(data = dict(SIN = [SIN]))
+glob_COS          = ColumnDataSource(data = dict(COS = [COS]))
+glob_g            = ColumnDataSource(data = dict(g = [g]))
+glob_alpha        = ColumnDataSource(data = dict(alpha = [alpha]))
+glob_t            = ColumnDataSource(data = dict(t = [t]))
 
 
 def init():
@@ -59,9 +59,9 @@ def init():
         Y.append((1-cos(pi*i/9))-1)
         X[0].append(cos(pi/4.0)*sqrt(1-Y[i]*Y[i]))
         X[1].append(-X[0][i])
-    SphereXLines[0]=np.array(X[0])
-    SphereXLines[1]=np.array(X[1])
-    SphereYLines=np.array(Y)
+    SphereXLines[0] = np.array(X[0])
+    SphereXLines[1] = np.array(X[1])
+    SphereYLines    = np.array(Y)
     glob_SphereXLines.data = dict(SphereXLines = [SphereXLines])
     glob_SphereYLines.data = dict(SphereYLines = [SphereYLines])
     # create the objects
@@ -87,17 +87,17 @@ def createSphere(r,sphere_data,sphere_lines_data):
     [SIN]          = glob_SIN.data["SIN"]                   # input/
     [COS]          = glob_COS.data["COS"]                   # input/
     # find the centre, knowing that it touches the ramp at (offset,H)
-    newX=offset+r*SIN
-    newY=H+r*COS
+    newX = offset+r*SIN
+    newY = H+r*COS
     # draw the sphere in blue
     sphere_data.data=dict(x=[newX],y=[newY],w=[2*r],c=["#0065BD"],a=[1])
     # use the referece lines to find the current position of the lines
-    RCOS=r*COS
-    RSIN=r*SIN
-    X1=SphereXLines[0]*RCOS+SphereYLines*RSIN+newX
-    X2=SphereXLines[1]*RCOS+SphereYLines*RSIN+newX
-    Y1=-SphereXLines[0]*RSIN+SphereYLines*RCOS+newY
-    Y2=-SphereXLines[1]*RSIN+SphereYLines*RCOS+newY
+    RCOS = r*COS
+    RSIN = r*SIN
+    X1 = SphereXLines[0]*RCOS+SphereYLines*RSIN+newX
+    X2 = SphereXLines[1]*RCOS+SphereYLines*RSIN+newX
+    Y1 = -SphereXLines[0]*RSIN+SphereYLines*RCOS+newY
+    Y2 = -SphereXLines[1]*RSIN+SphereYLines*RCOS+newY
     # draw the lines
     sphere_lines_data.data=dict(x=[X1, X2],y=[Y1,Y2])
 
@@ -115,17 +115,17 @@ def moveSphere(t,r,m,sphere_data,sphere_lines_data):
     # find the rotation of the sphere
     rotation = -displacement/r
     # find the new centre of the sphere
-    newX=displacement*COS+offset+r*SIN
-    newY=H-displacement*SIN+r*COS
+    newX = displacement*COS+offset+r*SIN
+    newY = H-displacement*SIN+r*COS
     # update the drawing
     sphere_data.data=dict(x=[newX],y=[newY],w=[2*r],c=["#0065BD"],a=[1])
     # find the new positions of the guidelines from the reference sphere
-    cosAngle=r*cos(alpha-rotation)
-    sinAngle=r*sin(alpha-rotation)
-    X1=SphereXLines[0]*cosAngle+SphereYLines*sinAngle+newX
-    X2=SphereXLines[1]*cosAngle+SphereYLines*sinAngle+newX
-    Y1=-SphereXLines[0]*sinAngle+SphereYLines*cosAngle+newY
-    Y2=-SphereXLines[1]*sinAngle+SphereYLines*cosAngle+newY
+    cosAngle = r*cos(alpha-rotation)
+    sinAngle = r*sin(alpha-rotation)
+    X1 = SphereXLines[0]*cosAngle+SphereYLines*sinAngle+newX
+    X2 = SphereXLines[1]*cosAngle+SphereYLines*sinAngle+newX
+    Y1 = -SphereXLines[0]*sinAngle+SphereYLines*cosAngle+newY
+    Y2 = -SphereXLines[1]*sinAngle+SphereYLines*cosAngle+newY
     
     sphere_lines_data.data=dict(x=[X1, X2],y=[Y1,Y2])
     return (newX,newY)
@@ -138,17 +138,17 @@ def createHollowSphere(r,sphere_data,sphere_lines_data):
     [SIN]          = glob_SIN.data["SIN"]                   # input/
     [COS]          = glob_COS.data["COS"]                   # input/
     # find the centre, knowing that it touches the ramp at (offset,H)
-    newX=offset+r*SIN
-    newY=H+r*COS
+    newX = offset+r*SIN
+    newY = H+r*COS
     # draw the sphere in semi-transparent blue
     sphere_data.data=dict(x=[newX],y=[newY],w=[2*r],c=["#0065BD"],a=[0.4])
     # use the referece lines to find the current position of the lines
-    RCOS=r*COS
-    RSIN=r*SIN
-    X1=SphereXLines[0]*RCOS+SphereYLines*RSIN+newX
-    X2=SphereXLines[1]*RCOS+SphereYLines*RSIN+newX
-    Y1=-SphereXLines[0]*RSIN+SphereYLines*RCOS+newY
-    Y2=-SphereXLines[1]*RSIN+SphereYLines*RCOS+newY
+    RCOS = r*COS
+    RSIN = r*SIN
+    X1 = SphereXLines[0]*RCOS+SphereYLines*RSIN+newX
+    X2 = SphereXLines[1]*RCOS+SphereYLines*RSIN+newX
+    Y1 = -SphereXLines[0]*RSIN+SphereYLines*RCOS+newY
+    Y2 = -SphereXLines[1]*RSIN+SphereYLines*RCOS+newY
     # draw the lines
     sphere_lines_data.data=dict(x=[X1, X2],y=[Y1,Y2])
 
@@ -167,17 +167,17 @@ def moveHollowSphere(t,r,m,ri,sphere_data,sphere_lines_data):
     # find the displacement of the point touching the ramp
     displacement = temp*r
     # find the new centre of the sphere
-    newX=displacement*COS+offset+r*SIN
-    newY=H-displacement*SIN+r*COS
+    newX = displacement*COS+offset+r*SIN
+    newY = H-displacement*SIN+r*COS
     # update the drawing
     sphere_data.data=dict(x=[newX],y=[newY],w=[2*r],c=["#0065BD"],a=[0.4])
     # find the new positions of the guidelines from the reference sphere
-    cosAngle=r*cos(alpha-rotation)
-    sinAngle=r*sin(alpha-rotation)
-    X1=SphereXLines[0]*cosAngle+SphereYLines*sinAngle+newX
-    X2=SphereXLines[1]*cosAngle+SphereYLines*sinAngle+newX
-    Y1=-SphereXLines[0]*sinAngle+SphereYLines*cosAngle+newY
-    Y2=-SphereXLines[1]*sinAngle+SphereYLines*cosAngle+newY
+    cosAngle = r*cos(alpha-rotation)
+    sinAngle = r*sin(alpha-rotation)
+    X1 = SphereXLines[0]*cosAngle+SphereYLines*sinAngle+newX
+    X2 = SphereXLines[1]*cosAngle+SphereYLines*sinAngle+newX
+    Y1 = -SphereXLines[0]*sinAngle+SphereYLines*cosAngle+newY
+    Y2 = -SphereXLines[1]*sinAngle+SphereYLines*cosAngle+newY
     sphere_lines_data.data=dict(x=[X1, X2],y=[Y1,Y2])
     return (newX,newY)
 
@@ -204,10 +204,10 @@ def moveCylinder(t,r,m, cylinder_data, cylinder_lines_data):
     # find the rotation of the cylinder
     rotation = -displacement/r
     # find the new centre of the cylinder
-    newX=displacement*COS+offset+r*SIN
-    newY=H-displacement*SIN+r*COS
-    cosRAngle=r*cos(alpha-rotation)
-    sinRAngle=r*sin(alpha-rotation)
+    newX      = displacement*COS+offset+r*SIN
+    newY      = H-displacement*SIN+r*COS
+    cosRAngle = r*cos(alpha-rotation)
+    sinRAngle = r*sin(alpha-rotation)
     # update the drawing
     cylinder_data.data=dict(x=[newX],y=[newY],w=[2*r],c=["#0065BD"],a=[1])
     cylinder_lines_data.data=dict(x=[[newX+cosRAngle,newX-cosRAngle],
@@ -245,15 +245,15 @@ def moveHollowCylinder(t,r,m,ri,hollowCylinder_data,hollowCylinder_lines_data):
     # find the displacement of the point touching the ramp
     displacement = r*temp
     # constants used multiple times calculated in advance to reduce computation time
-    cosAR=cos(alpha-rotation)
-    sinAR=sin(alpha-rotation)
-    cosRAngle=r*cosAR
-    cosRIAngle=ri*cosAR
-    sinRAngle=r*sinAR
-    sinRIAngle=ri*sinAR
+    cosAR      = cos(alpha-rotation)
+    sinAR      = sin(alpha-rotation)
+    cosRAngle  = r*cosAR
+    cosRIAngle = ri*cosAR
+    sinRAngle  = r*sinAR
+    sinRIAngle = ri*sinAR
     # find the new centre of the cylinder
-    newX=displacement*COS+offset+r*SIN
-    newY=H-displacement*SIN+r*COS
+    newX = displacement*COS+offset+r*SIN
+    newY = H-displacement*SIN+r*COS
     # update the drawing
     hollowCylinder_data.data=dict(x=[newX,newX],
         y=[newY,newY],w=[2*r,2*ri],c=["#0065BD","#FFFFFF"],a=[1,1])
@@ -269,9 +269,9 @@ def moveHollowCylinder(t,r,m,ri,hollowCylinder_data,hollowCylinder_lines_data):
 
 ## draw 3 graphs each containing a ramp, the angle marker, an ellipse, and lines
 
-XStart=-rampLength-maxR-5
-YEnd=H+2*maxR
-Width=-220.0*XStart/YEnd
+XStart = -rampLength-maxR-5
+YEnd   = H+2*maxR
+Width  = -220.0*XStart/YEnd
 fig1 = figure(title="Sphere",x_range=(XStart,0),y_range=(0,YEnd),height=220,width=int(Width))
 fig1.ellipse(x='x',y='y',width='w',height='w',fill_color='c',fill_alpha='a',
     line_color="#003359",line_width=3,source=fig1_data)
@@ -404,10 +404,10 @@ def changeAlpha(attr,old,new):
         X.append(-3*cos(i*alpha/10.0))
         Y.append(3*sin(i*alpha/10.0))
     AngleMarkerSource.data=dict(x=X,y=Y)
-    COS=cos(alpha)
-    SIN=sin(alpha)
-    offset=-rampLength*COS
-    H = rampLength*SIN
+    COS    = cos(alpha)
+    SIN    = sin(alpha)
+    offset = -rampLength*COS
+    H      = rampLength*SIN
     ramp_source.data = dict(x=[offset,0],y=[H,0])
     reset()
 
