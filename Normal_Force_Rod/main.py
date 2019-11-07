ffb1ff10c342765983a34e54e0fc5aeaaffbbb32
Matthias <39389064+m3bert@users.noreply.github.com>
2019-11-07 11:11:01 +0100

    reset to basic format
    bridge pattern slows down too much
    debugging difficult

--- Normal_Force_Rod/main.py
+++ Normal_Force_Rod/main.py
@@ -5,7 +5,7 @@ from bokeh.plotting import Figure#, output_file , show
 from bokeh.models import Arrow, OpenHead, LabelSet
 from bokeh.models.glyphs import ImageURL, Patch, MultiLine, Rect #, Quadratic, Rect, Patch
 from bokeh.models.layouts import Spacer
-from bokeh.models.widgets import Paragraph
+from bokeh.models.widgets import Paragraph, Button, CheckboxGroup, RadioButtonGroup, RadioGroup
 from bokeh.layouts import column, row, widgetbox, layout
 from bokeh.io import curdoc
 #import numpy as np
@@ -15,7 +15,7 @@ import sys, inspect
 currentdir = dirname(abspath(inspect.getfile(inspect.currentframe())))
 parentdir = join(dirname(currentdir), "shared/")
 sys.path.insert(0,parentdir)
-from latex_support import LatexDiv#, LatexLabel, LatexLabelSet, LatexSlider, LatexLegend
+from latex_support import LatexDiv, LatexLabel, LatexLabelSet, LatexSlider, LatexLegend
 
 
 
@@ -25,205 +25,31 @@ from NFR_constants import (
         lb, ub
         )
 
-from NFR_Shapes import (
-        NFR_Rod, NFR_RodShadow,
-        NFR_ForceArrow, NFR_ConstantLoad, NFR_TriangularLoad, NFR_TemperatureLoad,
-        NFR_Labels,
-        NFR_GraphN, NFR_GraphU
-        )
-from NFR_DrawAPI import (
-        NFR_BlueRod, NFR_BlackShadowRod, NFR_BlueArrow, NFR_BlueLoad,
-        NFR_BlackLabelText,
-        NFR_GreenGraph
-        )
-
-from NFR_GUIControl import NFR_GUIControl
 
 
-from NFR_helper_functions import (
-        set_load, 
-        refresh_objects,
-        compute_new_scenario
-        )
-
-
-########################################
-#####      init all objects        #####
-########################################
-rod = NFR_Rod(NFR_BlueRod())
-rod_shadow = NFR_RodShadow(NFR_BlackShadowRod())
+# ----------------------------------------------------------------- #
 
 
 
-
-# buttons, sliders, etc.
-control = NFR_GUIControl()
+radio_button_group = RadioButtonGroup(labels=["Point Load", "Constant Load", "Triangular Load", "Temperature"], active=0, width = 600)
 
 
 
-#TODO: set corect initial values
-# for main plot
-force_arrow = NFR_ForceArrow(NFR_BlueArrow(), xr_start-0.5, xr_start+0.5, 0.2, 0.2)
-const_load  = NFR_ConstantLoad(NFR_BlueLoad(), control.load_position_slider.value, lb, ub)
-triang_load = NFR_TriangularLoad(NFR_BlueLoad(), control.load_position_slider.value, lb, ub)
-temp_load   = NFR_TemperatureLoad(NFR_BlueLoad(), control.load_position_slider.value, lb, ub)
+radio_group_left  = RadioGroup(labels=["fixed", "sliding"], active=0, inline=True)
+radio_group_right = RadioGroup(labels=["fixed", "sliding"], active=1, inline=True)
+#radio_group_cross = RadioGroup(labels=["constant", "tapered"], active=0, inline=True) # cross-section
+radio_group_ampl  = RadioGroup(labels=["-1", "+1"], active=1, inline=True) # amplitude
 
-labels      = NFR_Labels(NFR_BlackLabelText(), control.load_position_slider.value)
-
-obj_list = [force_arrow, const_load, triang_load, temp_load, labels]
-
-# for N and U plot
-graph_N  = NFR_GraphN(NFR_GreenGraph())
-graph_U  = NFR_GraphU(NFR_GreenGraph())
+# Reset Button
+reset_button = Button(label="Reset", button_type="success")
+line_button  = Button(label="Show line", button_type="success")
 
 
 
+load_position_slider  = LatexSlider(title="\\mathrm{Load \ Position}", value_unit='\\frac{\\mathrm{L}}{\\mathrm{10}}', value=(xr_end-xr_start)/2, start=xr_start, end=xr_end, step=1.0)
 
 
 
-########################################
-#####      init all objects        #####
-########################################
-# seems impossible to outsurce using this structure
-# keep them as short and simple as possible
-# helper functions can be used in the way that they don't manipulate CDS directly
-
-def change_load(attr, old, new):
-    print("DEBUG: change_load, new=",new)
-    current_position = control.load_position_slider.value
-    
-#    #print(const_load.shape.data)
-#    [force_arrow.shape.data,
-#     const_load.shape.data,
-#     triang_load.shape.data,
-#     temp_load.shape.data]    =  set_load(new,current_position)
-    
-    
-    #print(const_load.shape.data)
-    
-    #obj_list = [force_arrow, const_load, triang_load, temp_load]
-    set_load(new, current_position, obj_list)
-    
-    #print(const_load.shape.data)
-    
-#    force_arrow.draw(plot_main)
-#    const_load.draw(plot_main)
-#    triang_load.draw(plot_main)
-#    temp_load.draw(plot_main)
-    
-    refresh_objects(obj_list, plot_main)
-    
-    
-    #const_load.drawAPI.drawPatch(plot_main, const_load.shape, alpha=0.1)
-    #print(const_load.shape.data)
-    compute_new_scenario(control, graph_N, graph_U)
-    graph_N.draw(plot_normalF)
-    graph_U.draw(plot_deform)
-    
-    
-    
-def change_load_position(attr, old, new):
-    
-    # have to handle distinct cases, otherwise there are ValueErrors for empty lists
-    new_position = new*10/(xr_end-xr_start)
-    
-    current_load = control.radio_button_group.active
-    
-    set_load(current_load, new_position, obj_list)
-    refresh_objects(obj_list, plot_main)
-    #print("DBUG: change loaod pos")
-    compute_new_scenario(control, graph_N, graph_U)
-    graph_N.draw(plot_normalF)
-    graph_U.draw(plot_deform)
-
-
-
-def change_left_support(attr, old, new):
-    # new==0 means fixed support image
-    # new==1 means slide support image
-#    new_support_img = fixed_support_img if new==0 else slide_support_img
-#    support_source_left.data = dict(sp_img=[new_support_img], x=[xsl] , y=[ysl])
-#    # TODO: check again if it is possible to only change sp_img
-#    
-#    if radio_group_right.active==1 and new==1: # both slide
-#        show_error(True)
-#    else:
-#        show_error(False)
-        
-    compute_new_scenario(control, graph_N, graph_U)
-    graph_N.draw(plot_normalF)
-    graph_U.draw(plot_deform)
-
-
-
-def change_right_support(attr, old, new):
-    # new==0 means fixed support image
-    # new==1 means slide support image
-#    new_support_img = fixed_support_img if new==0 else slide_support_img
-#    support_source_right.data = dict(sp_img=[new_support_img], x=[xsr] , y=[ysr])
-#    # TODO: check again if it is possible to only change sp_img
-#    
-#    if radio_group_left.active==1 and new==1: # both slide
-#        show_error(True)
-#    else:
-#        show_error(False)
-        
-    compute_new_scenario(control, graph_N, graph_U)
-    graph_N.draw(plot_normalF)
-    graph_U.draw(plot_deform)
-
-
-
-
-def change_amplitude(attr, old, new):
-    xS_old = force_arrow.shape.data["xS"]
-    xE_old = force_arrow.shape.data["xE"]
-    
-    # change direction of arrows in x-direction (parallel to rod)
-    force_arrow.shape.data["xS"] = xE_old
-    force_arrow.shape.data["xE"] = xS_old
-    
-    # change sign for the calculations
-    #global_variables["ampl"] = -global_variables["ampl"]
-    #print("DBUG: change_ampl")
-    compute_new_scenario(control, graph_N, graph_U)
-    graph_N.draw(plot_normalF)
-    graph_U.draw(plot_deform)
-
-
-
-def reset():
-    control.radio_button_group.active = 0
-    control.radio_group_left.active   = 0
-    control.radio_group_right.active  = 1
-    #radio_group_cross.active  = 0
-    control.radio_group_ampl.active   = 1
-    control.load_position_slider.value = (xr_end-xr_start)/2
-    set_load(control.radio_button_group.active,control.load_position_slider.value, obj_list)
-    compute_new_scenario(control, graph_N, graph_U)
-    graph_N.draw(plot_normalF)
-    graph_U.draw(plot_deform)
-
-
-
-
-
-control.radio_button_group.on_change('active',change_load)
-
-control.radio_group_left.on_change('active',change_left_support)
-control.radio_group_right.on_change('active',change_right_support)
-control.radio_group_ampl.on_change('active',change_amplitude)
-
-control.load_position_slider.on_change('value',change_load_position)
-
-control.reset_button.on_click(reset)
-
-
-
-
-########################################
-#####           PLOTTING           #####
-########################################
 
 
 ###### MAIN PLOT (SUPPORT AND LOAD):
@@ -235,14 +61,6 @@ plot_main.outline_line_width = 2
 plot_main.outline_line_color = "Black"
 plot_main.title.text_font_size = "13pt"
 plot_main.toolbar.logo = None
-
-
-#rod.drawAPI.drawPatch(plot_main, rod.shape)
-rod.draw(plot_main)
-force_arrow.draw(plot_main)
-#const_load.draw(plot_main)
-
-
 
 
 
@@ -257,7 +75,6 @@ plot_normalF.outline_line_color = "Black"
 plot_normalF.title.text_font_size = "13pt"
 plot_normalF.toolbar.logo = None
 
-rod_shadow.draw(plot_normalF)
 
 
 
@@ -271,10 +88,6 @@ plot_deform.outline_line_width = 2
 plot_deform.outline_line_color = "Black"
 plot_deform.title.text_font_size = "13pt"
 plot_deform.toolbar.logo = None
-
-
-rod_shadow.draw(plot_deform)
-
 
 
 
@@ -297,16 +110,17 @@ doc_layout = layout(children=[
         column(description,
                row(column(
                        Spacer(height=20,width=450),
-                       widgetbox(control.radio_button_group),
-                       row(widgetbox(p_rt1, width=120), widgetbox(control.radio_group_left)),
-                       row(widgetbox(p_rt2, width=120), widgetbox(control.radio_group_right)), 
+                       widgetbox(radio_button_group),
+                       row(widgetbox(p_rt1, width=120), widgetbox(radio_group_left)),
+                       row(widgetbox(p_rt2, width=120), widgetbox(radio_group_right)), 
                        ##row(widgetbox(p_rt3, width=120), widgetbox(radio_group_cross)), 
-                       row(widgetbox(p_rt4, width=120), widgetbox(control.radio_group_ampl)), 
-                       control.load_position_slider,
+                       row(widgetbox(p_rt4, width=120), widgetbox(radio_group_ampl)), 
+                       load_position_slider,
                        ##load_magnitude_slide,
                        ##slider_group,
                        #simple_button_group
-                       control.reset_button),
+                       reset_button,
+                       line_button),
                    column(plot_main,plot_normalF,plot_deform ) ) ) ] )
 
 
--- Normal_Force_Rod_old/NFR_helper_functions.py
+++ Normal_Force_Rod_old/NFR_helper_functions.py
@@ -1,65 +1,59 @@
 from __future__ import division # float division only, like in python 3
 
-## inner app imports
+
+import numpy as np
+
 from NFR_constants import (
-        xr_start, xr_end, # rod coords
-        y_offset,# y_cross,
-        xsl, xsr, ysl, ysr, # support coords
-        slide_support_img, fixed_support_img # support images
-        )
-from NFR_data_sources import (
-        rod_source, #global_variables,
-        support_source_left, support_source_right,
-        force_point_source, constant_load_source, triangular_load_source,
-        temperature_source,
-        labels_source, labels_N, labels_U,
-        aux_line, samplesF,
-        error_msg, error_msg_frame,
-        temp_pics
-        )
-from NFR_buttons import (
-        radio_group_left, radio_group_right, #radio_group_cross,
-        radio_group_ampl,
-        radio_button_group,
-        load_position_slide,
-        line_button
-        )
-from NFR_equations import (
-        calcNU
+        xr_start, y_offset,
+        lb, ub,
+        xr_end, sol_reso
         )
 
+from NFR_equations import calcNU
 
 
 def clear_point_load():
-    force_point_source.data = dict(xS=[], xE=[], yS=[], yE=[], lW=[], lC=[])
+    return dict(xS=[], xE=[], yS=[], yE=[])
 def clear_constant_load():
-    constant_load_source.data = dict(x=[], y=[])
+    return dict(x=[], y=[])
 def clear_triangular_load():
-    triangular_load_source.data = dict(x=[], y=[])
+    return dict(x=[], y=[])
 def clear_temperature():
-    temperature_source.data = dict(x=[], y=[])
+    return dict(x=[], y=[])
 def clear_labels():
-    labels_source.data = dict(x=[], y=[], name=[])
-# TODO: check for general CDS clearing    
+    return dict(x=[], y=[], name=[])
 
 
 # TODO: maybe variable for arrow length for more general case?
     
 
-def set_point_load(load_position):
+def set_point_load(load_position, load_output):
     #y_cross = global_variables["y_cross"]
-    labels_source.data = dict(x=[xr_start-0.1+load_position, xr_start-0.05+load_position],y=[y_offset+0.3,y_offset],name=['F','|'])
-    force_point_source.data = dict(xS=[xr_start-0.5+load_position], xE=[xr_start+0.5+load_position], yS=[y_offset+0.2], yE=[y_offset+0.2], lW=[2], lC=["#0065BD"])
-    clear_constant_load()
-    clear_triangular_load()
-    clear_temperature()
+    # labels
+    load_output[4] = dict(x=[xr_start-0.1+load_position, xr_start-0.05+load_position],y=[y_offset+0.3,y_offset],name=['F','|'])
+    
+    load_output[0] = dict(xS=[xr_start-0.5+load_position], xE=[xr_start+0.5+load_position], yS=[y_offset+0.2], yE=[y_offset+0.2])
+    
+    load_output[1] = clear_constant_load()
+    load_output[2] = clear_triangular_load()
+    load_output[3] = clear_temperature()
 
-
-def set_constant_load(load_position):
+#def set_constant_load(load_position, load_output):
+#    #y_cross = global_variables["y_cross"]
+#    #labels_source.data = dict(x=[xr_start-0.1+load_position, xr_start-0.05+load_position],y=[y_offset+0.3,y_offset],name=['F','|'])
+#    
+#    #load_output[0] = dict(xS=[xr_start-0.5+load_position], xE=[xr_start+0.5+load_position], yS=[y_offset+0.2], yE=[y_offset+0.2], lW=[2], lC=["#0065BD"])
+#    load_output[0] = clear_point_load()
+#    load_output[1] = clear_constant_load()
+#    load_output[2] = clear_triangular_load()
+#    load_output[3] = clear_temperature()
+    
+    
+def set_constant_load(load_position, load_output):
     if load_position<1e-5: #close to zero
-        clear_constant_load()
-        clear_labels()
-        clear_point_load()
+        load_output[1] = clear_constant_load()
+        load_output[4] = clear_labels()
+        load_output[0] = clear_point_load()
     else:
         #labels_source.data = dict(x=[xr_start+1.5,xr_start+4.5,xr_start+7.5],y=[y_offset+0.9,y_offset+0.9,y_offset+0.9],name=['F','F','F'])
         #y_cross = global_variables["y_cross"]
@@ -79,21 +73,22 @@ def set_constant_load(load_position):
             xE.append(part*i)
         
         #labels_source.data = dict(x=xM,y=[y_offset+0.9,y_offset+0.9,y_offset+0.9],name=['F']*num_arrows)
-        labels_source.data = dict(x=[load_position+0.1],y=[y_offset+0.2], name=['p'])
+        load_output[4] = dict(x=[load_position+0.1],y=[y_offset+0.2], name=['p'])
         
-        force_point_source.data = dict(xS=xS, xE=xE, yS=[y_offset+0.45]*num_arrows, yE=[y_offset+0.45]*num_arrows, lW=[2]*num_arrows, lC=["#0065BD"]*num_arrows)
+        load_output[0] = dict(xS=xS, xE=xE, yS=[y_offset+0.45]*num_arrows, yE=[y_offset+0.45]*num_arrows)#, lW=[2]*num_arrows, lC=["#0065BD"]*num_arrows)
         
-        constant_load_source.data = dict(x=[xr_start, xr_start, load_position, load_position], y=[y_offset+0.2, y_offset+0.7, y_offset+0.7, y_offset+0.2])
+        load_output[1] = dict(x=[xr_start, xr_start, load_position, load_position], y=[y_offset+lb, y_offset+ub, y_offset+ub, y_offset+lb])
         #triangular_load_source.data = dict(x=[], y=[])
-    clear_triangular_load()
-    clear_temperature()
+    load_output[2] = clear_triangular_load()
+    load_output[3] = clear_temperature()
 
 
-def set_triangular_load(load_position):
+
+def set_triangular_load(load_position, load_output):
     if load_position<1e-5: #close to zero
-        clear_triangular_load()
-        clear_labels()
-        clear_point_load()
+        load_output[2] = clear_triangular_load()
+        load_output[4] = clear_labels()
+        load_output[0] = clear_point_load()
     else:
         #y_cross = global_variables["y_cross"]
         
@@ -109,127 +104,107 @@ def set_triangular_load(load_position):
             #xM.append(part*(i+0.5))
         for i in local_index[1:][::2]:
             xE.append(part*i)
-        labels_source.data = dict(x=[load_position+0.1],y=[y_offset+0.2], name=['p'])
-        force_point_source.data = dict(xS=xS, xE=xE, yS=[y_offset+0.45]*num_arrows, yE=[y_offset+0.45]*num_arrows, lW=[2]*num_arrows, lC=["#0065BD"]*num_arrows)
-        triangular_load_source.data = dict(x=[xr_start, xr_start, load_position], y=[y_offset+0.2, y_offset+0.7, y_offset+0.2])
-    clear_constant_load()
-    clear_temperature()
+        load_output[4] = dict(x=[load_position+0.1],y=[y_offset+0.2], name=['p'])
+        load_output[0] = dict(xS=xS, xE=xE, yS=[y_offset+0.45]*num_arrows, yE=[y_offset+0.45]*num_arrows)
+        load_output[2] =  dict(x=[xr_start, xr_start, load_position], y=[y_offset+lb, y_offset+ub, y_offset+lb])
+    load_output[1] = clear_constant_load()
+    load_output[3] = clear_temperature()
+    
 
 
-def set_temperature(load_position):
+def set_temperature(load_position, load_output):
     if load_position<1e-5: #close to zero
-        clear_temperature()
-        clear_labels()
+        load_output[3] = clear_temperature()
+        load_output[4] = clear_labels()
     else:
         #y_cross = global_variables["y_cross"]
         
-        labels_source.data = dict(x=[(load_position-xr_start)/2],y=[y_offset+0.35], name=['T'])
-        temperature_source.data = dict(x=[xr_start, xr_start, load_position, load_position], y=[y_offset+0.2, y_offset+0.7, y_offset+0.7, y_offset+0.2])
+        load_output[4] = dict(x=[(load_position-xr_start)/2],y=[y_offset+0.35], name=['T'])
+        load_output[3] = dict(x=[xr_start, xr_start, load_position, load_position], y=[y_offset+lb, y_offset+ub, y_offset+ub, y_offset+lb])
         #TODO: nice design for Temperature (hot/cold)
-    clear_point_load()
-    clear_constant_load()
-    clear_triangular_load()
+    load_output[0] = clear_point_load()
+    load_output[1] = clear_constant_load()
+    load_output[2] = clear_triangular_load()
     
     #"Normal_Force_Rod/static/images/snowflake01.svg"
-    xC = [1.2, 4.8, 7.1]
-    yC = [0.0, 0.0, 0.0]
-    temp_pics.data = dict(x=xC, y=yC, img=["Normal_Force_Rod/static/images/snowflake03.svg"]*3)
+    #xC = [1.2, 4.8, 7.1]
+    #yC = [0.0, 0.0, 0.0]
+    #temp_pics.data = dict(x=xC, y=yC, img=["Normal_Force_Rod/static/images/snowflake03.svg"]*3)
     #TODO: find the reason why the pictures don't show...
+
+
+
     
     
-
-
-
-def set_load(load_type, load_position):
+def set_load(load_type, load_position, obj_list):
+    load_output = [dict()]*(4+1)  # num of load types + labels
+    # empty dict is not enough, the variables in the CDS have to be set empty
+    # => load_ouput has to be filled by the specific functions
     if load_type==0:
-        set_point_load(load_position)
+        #load_output[0] = set_point_load(load_position)
+        set_point_load(load_position, load_output)
     elif load_type==1:
-        set_constant_load(load_position)
+        set_constant_load(load_position, load_output)
     elif load_type==2:
-        set_triangular_load(load_position)
+        set_triangular_load(load_position, load_output)
     elif load_type==3:
-        set_temperature(load_position)
+        set_temperature(load_position, load_output)
     else:
         print("How did you get here? [helper_functions, set_load]")
+    #return load_output
+    
+    for i, obj in enumerate(obj_list):
+        obj.shape.data = load_output[i]
+    
+
+
+
+
+def refresh_objects(obj_list,fig):
+    # give list of objects and loop over it to draw them
+    for obj in obj_list:
+        obj.draw(fig)
+
+
+
+
+
+def compute_new_scenario(GUI, graph_obj_N, graph_obj_U):
+    ls_type   = GUI.radio_group_left.active
+    rs_type   = GUI.radio_group_right.active
+    load_type = GUI.radio_button_group.active
+    L1        = GUI.load_position_slider.value
+    #print("DBUG: pos_slider:", L1)
+    ampl      = -1 + 2*GUI.radio_group_ampl.active  # ampl=-1 if active=0, ampl=1 if active=1
+    #print("DBUG: ampl:", ampl)
+    #samples   = calcNU(ls_type, rs_type, load_type, L1, graph_obj_N.shape.data['x'], ampl)
+    #x_samples = np.linspace(xr_start,xr_end,sol_reso)
+    #samples   = calcNU(ls_type, rs_type, load_type, L1, x_samples, ampl)
+    samples   = calcNU(ls_type, rs_type, load_type, L1, ampl)
+#    graph_obj_N.shape.data['x'] = samples['x']
+#    graph_obj_N.shape.data['y'] = samples['yN']
+#    graph_obj_U.shape.data['x'] = samples['x']
+#    graph_obj_U.shape.data['y'] = samples['yU']
+    graph_obj_N.shape.data = dict(x=samples['x'], y=samples['yN'])
+    graph_obj_U.shape.data = dict(x=samples['x'], y=samples['yU'])
+    
+    print("in comp...")
+    print("DBUG: yN=",samples['yN'])
+    print("DBUG: yU=",samples['yU'])
+    print("DBUG: CDSyN=",graph_obj_N.shape.data['y'])
+    print("DBUG: CDSyU=",graph_obj_U.shape.data['y'])
+    print("comp fertig...")
+#    show_special_values()
+#    # if the auxiliary line should be shown, the line button shows "Hide line"
+#    if line_button.label == "Hide line":
+#        move_aux_line()
+
+
+
+
+
+
+
+
     
     
-    #TODO: consider re-structuring with function handles like in rolling test
-    # advantage: calling the functions via list index instead of if-else
-
-
-
-
-def move_aux_line():
-    x_samples = samplesF.data['x']
-    y_samples = samplesF.data['y']
-    roots     = []
-    
-    for i in range(0,len(y_samples)-1):
-        if y_samples[i]*y_samples[i+1] < 0: # sign changes
-            r = 0.5*(x_samples[i+1]-x_samples[i]) + x_samples[i]
-            roots.append([r,r])
-    
-    aux_line.data = dict(x=roots, y=[[15,-15]]*len(roots))
-
-
-
-def show_error(show=True):
-    
-    if show:
-        error_msg.data = dict(x=[2],y=[1.35],name=["Warning! - Kinematic, rod slides away!"])
-        error_msg_frame.data = dict(x=[5], y=[1.5])
-    else:
-        error_msg.data = dict(x=[], y=[], name=[])
-        error_msg_frame.data = dict(x=[], y=[])
-
-
-
-def compute_new_scenario():
-    ls_type   = radio_group_left.active
-    rs_type   = radio_group_right.active
-    load_type = radio_button_group.active
-    L1        = load_position_slide.value
-    calcNU(ls_type, rs_type, load_type, L1)
-    show_special_values()
-    # if the auxiliary line should be shown, the line button shows "Hide line"
-    if line_button.label == "Hide line":
-        move_aux_line()
-
-
-def show_special_values(): #labels
-    # show only for fixed-fixed scenario
-    if (radio_group_left.active == 0 and radio_group_right.active == 0):
-        load_type = radio_button_group.active
-        
-        # for constant and triangular load only for Load Poistion == L
-        if (load_position_slide.value == xr_end):
-            if load_type==1: # constant load
-                labels_N.data = dict(x=[0.0,10.0], y=[2.0,-2.0], name=['N', 'N'])
-                labels_U.data = dict(x=[5.0], y=[10.0], name=['U'])
-            elif load_type==2: # triangular load
-                labels_N.data = dict(x=[0.0,10.0], y=[2.0,-2.0], name=['N', 'N'])
-                labels_U.data = dict(x=[6.0], y=[8.0], name=['U'])
-        
-        # for point load only for Load Position = L/2
-        elif (load_position_slide.value == (xr_end-xr_start)//2):
-            if load_type==0: # point load
-                labels_N.data = dict(x=[0.0,10.0], y=[2.0,-2.0], name=['N', 'N'])
-                labels_U.data = dict(x=[5.0], y=[-5.0], name=['U'])
-        else:
-            labels_N.data = dict(x=[], y=[], name=[])
-            labels_U.data = dict(x=[], y=[], name=[])
-            
-    else:
-        labels_N.data = dict(x=[], y=[], name=[])
-        labels_U.data = dict(x=[], y=[], name=[])
-        #TODO: clear by function 
-
-
-#labels_N.data = dict(x=[0.0,10.0], y=[2.0,2.0], name=['N', 'N'])
-#labels_U.data = dict(x=[5.0], y=[-5.0], name=['U'])
-
-
-
-
-
-
