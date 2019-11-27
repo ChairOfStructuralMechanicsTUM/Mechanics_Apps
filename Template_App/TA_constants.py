a1ad01c67848f30cb7d5086e06f0bcb03bd4c580
Matthias <39389064+m3bert@users.noreply.github.com>
2019-11-27 13:02:39 +0100

    add images

--- Template_App/TA_constants.py
+++ Template_App/TA_constants.py
@@ -9,36 +9,15 @@ Template App - constants used throughout all files
 #           coordinates           #
 ###################################
 
-# rod/beam
-xr_start = 0.0
-xr_end   = 10.0
-y_offset = 0.0
-#y_cross  = 0.5 # offset for the labels etc. in case of tapared cross-section mode
-r_reso   = 100  # resolution, i.e. number of points to draw the rod
-sol_reso = 1000 # resolution/samples for the N and U functions
-
 # supports
-xsl      = xr_start - 0.325
-xsr      = xr_end - 0.33
-ysl      = -0.05
-ysr      = -0.08
+xsl      = 0
+xsr      = 4
+ysl      = 0
+ysr      = 0
 
 # load patch bounds
 lb       = 0.2   # lower boundary
 ub       = 0.7   # upper boundary
-
-
-## constants used for the functions/equations
-F        = 1.0
-L        = xr_end-xr_start
-E        = 1.0
-A        = 1.0
-p0       = 1.0
-T        = 1.0
-alpha_T  = 1.0
-sigma    = 0.0   # actually no sigma in formulas, missread; could be replaced by hard coded zeros
-
-
 
 
 ###################################
@@ -48,11 +27,15 @@ sigma    = 0.0   # actually no sigma in formulas, missread; could be replaced by
 # scale
 fig_width  = 630
 fig_height = 300
-x_range    = (xr_start-1.5,xr_end+1.5)
+#x_range    = (xr_start-1.5,xr_end+1.5)
 
 # color
 color_rod   = "#0065BD" # beam color
 color_arrow = "#0065BD" # force arrow color
+
+# support scales
+support_width  = 0.66
+support_height = 0.4
 
 
 
@@ -75,5 +58,5 @@ button_width = 100
 ##################################
 
 # images/graphics from external sources
-slide_support_img = "Normal_Force_Rod/static/images/slide_support.svg"
-fixed_support_img = "Normal_Force_Rod/static/images/fixed_support.svg"
+slide_support_img = "Template_App/static/images/slide_support.svg"
+fixed_support_img = "Template_App/static/images/fixed_support.svg"
--- Template_App/main.py
+++ Template_App/main.py
@@ -6,13 +6,18 @@ import numpy as np
 
 # bokeh imports
 from bokeh.io             import curdoc
-from bokeh.plotting       import Figure
+from bokeh.plotting       import figure
 from bokeh.models         import ColumnDataSource, LabelSet, Arrow, OpenHead
-from bokeh.models.glyphs  import MultiLine, Rect ,ImageURL #, Patch, 
+from bokeh.models.glyphs  import MultiLine, Rect, ImageURL #, Patch, 
 from bokeh.models.widgets import Paragraph, Button, RadioButtonGroup, RadioGroup #CheckboxGroup
 from bokeh.layouts        import column, row, widgetbox, layout, Spacer
 
 # internal imports
+from TA_constants import (
+    slide_support_img, fixed_support_img,
+    xsl, ysl, xsr, ysr,
+    support_width, support_height
+)
 
 # latex integration
 from os.path import dirname, join, split, abspath
@@ -27,11 +32,31 @@ from latex_support import LatexDiv, LatexLabel, LatexLabelSet, LatexSlider, Late
 ###############################
 #      ColumnDataSources      #
 ###############################
+cds_support_left  = ColumnDataSource(data=dict(sp_img=[fixed_support_img], x=[xsl] , y=[ysl]))
+cds_support_right = ColumnDataSource(data=dict(sp_img=[slide_support_img], x=[xsr] , y=[ysr]))
 
 
 
 
 
+###################################
+#             Figures             #
+###################################
+
+### define the figure ###
+# the shown attributes should always be set
+# if no tool is needed set tools="" or toolbar_location=None
+# for more attributes have a look at the bokeh documentation
+figure_name = figure(title="Example Figure", x_range=(-1,5), y_range=(-0.5,2.5), height=300, width=400, tools="pan, wheel_zoom, reset")
+figure_name.toolbar.logo = None # do not display the bokeh logo
+
+
+### add the support images ###
+# urls and coordinates are provided by a ColumnDataSource
+# anchor specifies at which position of the image the x and y coordinates are referring to
+# width and height could also be set using constants defined in TA_constants.py and imported here in main.py
+figure_name.add_glyph(cds_support_left,  ImageURL(url="sp_img", x='x', y='y', w=0.66, h=0.4, anchor="center"))
+figure_name.add_glyph(cds_support_right, ImageURL(url="sp_img", x='x', y='y', w=support_width, h=support_height, anchor="center"))
 
 
 
@@ -44,7 +69,8 @@ description = LatexDiv(text=open(description_filename).read(), render_as_text=Fa
 
 
 curdoc().add_root(column(
-    description
+    description,
+    figure_name
 ))
 curdoc().title = split(dirname(__file__))[-1].replace('_',' ').replace('-',' ')  # get path of parent directory and only use the name of the Parent Directory for the tab name. Replace underscores '_' and minuses '-' with blanks ' '
 
