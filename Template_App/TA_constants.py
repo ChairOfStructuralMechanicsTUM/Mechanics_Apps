de6dfb4bcfaaf4ac37a0d577e8e035f86707e86a
Matthias <39389064+m3bert@users.noreply.github.com>
2019-12-02 09:38:53 +0100

    add official colors

--- Template_App/TA_constants.py
+++ Template_App/TA_constants.py
@@ -4,6 +4,27 @@ Template App - constants used throughout all files
 
 # ----------------------------------------------------------------- #
 
+###################################
+#             colors              #
+###################################
+
+# c for "color" or "custom" to avoid possible confusion with standard colors
+
+# basic colors
+c_black = "#333333"
+c_blue  = "#3070b3"
+c_white = "#ffffff"
+
+# arrows / objects
+c_green  = "#a2ad00 "
+c_orange = "#e37222"
+
+# auxiliary lines / objects
+c_gray        = "#b3b3b3"
+c_gray_text   = "#8a8a8a"
+c_gray_light1 = "#e6e6e6"
+c_gray_light2 = "#f7f7f7"
+c_gray_warm   = "#8f8f8f"
 
 ###################################
 #           coordinates           #
