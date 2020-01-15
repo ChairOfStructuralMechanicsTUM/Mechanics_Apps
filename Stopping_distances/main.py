95e52e6e66bad995e06386476933cc31242e4151
m3bert <39389064+m3bert@users.noreply.github.com>
2020-01-15 15:29:35 +0100

    Update main.py

--- Stopping_distances/main.py
+++ Stopping_distances/main.py
@@ -10,7 +10,7 @@ sys.path.insert(0,parentdir)
 from latex_support import LatexDiv
 
 from bokeh.io import curdoc
-from bokeh.layouts import column, row#test
+from bokeh.layouts import column, row
 
 
 # create each part of the window
